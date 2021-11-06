3435744201a4833e32ede254c973f1829216b305
AbrahamWang484 <wang.ibrahim222@gmail.com>
2021-11-06 08:45:31 -0700

    add booleanExample file

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/BooleanExample.java
@@ -0,0 +1,9 @@
+package org.la.core.java.student.datatype.wang;
+
+public class BooleanExample {
+	public static void main(String[] args) {
+		boolean isMale=true;
+		System.out.println("I am a Male : "+isMale);
+	}
+
+}
