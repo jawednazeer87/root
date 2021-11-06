77017aad1620eee3ba03db096bbaf9e9b8b449dc
jawednazeer87 <jawednazeer87@gmail.com>
2021-11-06 23:26:25 +0800

    data type discussion

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/jawed/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.datatype.jawed;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("First java class");
+	}
+
+}
