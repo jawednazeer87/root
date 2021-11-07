abbab4194b911b95fed46426d7a62a930e79c96f
AbrahamWang484 <wang.ibrahim222@gmail.com>
2021-11-06 18:19:09 -0700

    add boolean file test on boolean

--- core-java/src/main/java/org/la/core/java/student/datatype/wang/BooleanExample.java
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/BooleanExample.java
@@ -2,6 +2,7 @@ package org.la.core.java.student.datatype.wang;
 
 public class BooleanExample {
 	public static void main(String[] args) {
+		//boolean only has two values : true * false
 		boolean isMale=true;
 		System.out.println("I am a Male : "+isMale);
 	}
