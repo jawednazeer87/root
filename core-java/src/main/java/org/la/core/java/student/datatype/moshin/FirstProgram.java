a879a9e361e6754f2576fc1c74d431585caf0b31
moshin <moshin.ma715@gmail.com>
2021-11-12 18:47:07 +0530

    Data Type Programs

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/moshin/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.datatype.moshin;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("First Java Program");
+	}
+
+}
