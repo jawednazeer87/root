be367bbc5949583fd873fae7cfd702cbc5018dbc
sajidmohammed90 <sjmohammed90@gmail.com>
2021-11-12 07:40:08 +0530

    Two Integer variable and it's value

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/SjMohammed/SecondExample.java
@@ -0,0 +1,30 @@
+package org.la.core.java.student.datatype.SjMohammed;
+
+public class SecondExample {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=2;
+		long c=30;
+		long d=100;
+		float e=10.0f;
+		float f=20.5f;
+		double g=100.95;
+		double h=500.65;
+		byte i=3;
+		byte j=10;
+		boolean k=true;
+		boolean l=false;
+		char m='S';
+		char n='A';
+		System.out.println("Two integer variable and its value :"+a +" "+ +b);
+		System.out.println("Two Long variable and its value :"+c +" "+ +d);
+		System.out.println("Two Float variable and its value :"+e +" "+ +f);
+		System.out.println("Two Double variable and its value :"+g +" "+ +h);
+		System.out.println("Two Byte variable and its value :"+i +" "+ +j);
+		System.out.println("Boolean variable and its value :"+k );
+		System.out.println("Boolean variable and its value :"+l );
+		System.out.println("Two Char variable and its value :"+m +" "+ +n);
+	}
+
+}
