e07dff457c435ede62d1d3eb21fa807afd64e1e8
sajidmohammed90 <sjmohammed90@gmail.com>
2021-11-12 07:15:25 +0530

    DataType

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/SjMohammed/FirstProgram.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.SjMohammed;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Java World");
+		System.out.println("Java Class");
+		System.out.println("Java Classes");
+		System.out.println("Sotware Program");
+		System.out.println("Software Classes");
+	}
+
+}
