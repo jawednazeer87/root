a4158ce445537a4a72d3a3bd8b0a83a2bd47b986
AbrahamWang484 <wang.ibrahim222@gmail.com>
2021-11-13 08:09:43 -0800

    update

--- core-java/src/main/java/org/la/core/java/student/datatype/wang/RetionalLogicalOperator.java
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/RetionalLogicalOperator.java
@@ -1,5 +1,19 @@
 package org.la.core.java.student.datatype.wang;
 
 public class RetionalLogicalOperator {
-
+public static void main(String[] args) {
+	//Relational return boolean only
+			boolean d= 1==3;
+			System.out.println("d = "+d);
+			
+			boolean e= a!=b;
+			System.out.println("e = "+ e);
+			
+			//Logical : && || !(not)
+			int x=1, y=2;
+			boolean b= x==y && y++==3;
+			System.out.println("x = "+x);
+			System.out.println("y = "+y);
+			System.out.println("b = "+b);
+}
 }
