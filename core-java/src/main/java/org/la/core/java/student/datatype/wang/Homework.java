f090ff852b040b09b754378685b8c0415994fbdb
AbrahamWang484 <wang.ibrahim222@gmail.com>
2021-11-13 07:03:44 -0800

    update the homework

--- core-java/src/main/java/org/la/core/java/student/datatype/wang/Homework.java
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/Homework.java
@@ -73,9 +73,9 @@ public class Homework {
 		System.out.println(v);
 		System.out.println(w);
 //		22. wap declare float and double variables and print its value
-		float x=8.8F;
+		float xx=8.8F;
 		double y=1.8;
-		System.out.println(f);
+		System.out.println(xx);
 		System.out.println(y);
 //		23. wap declare float and char variables and print its value
 		float z=3.3F;
@@ -214,12 +214,13 @@ public class Homework {
 //		50. wap declare 2 boolean, 3 int and 1 long variable and assign value and print it
 		boolean cs=true,ct=false;
 		int co=666,cp=777,cq=888;
-		long cr=123345364234L;
+		long crr=123345364234L;
 		System.out.println(cs);
 		System.out.println(ct);
 		System.out.println(co);
 		System.out.println(cp);
 		System.out.println(cq);
+		System.out.println(crr);
 	}
 
 }
