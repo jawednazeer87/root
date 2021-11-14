69139d49b0831303f15a7b26d68918708776011d
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-11-14 14:16:58 +0530

    1st Assignment Arfaz

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/program/arfaz/FiftyProgram.java
@@ -0,0 +1,160 @@
+package org.la.core.java.student.datatype.program.arfaz;
+
+public class FiftyProgram {
+
+	public static void main(String[] args) {
+System.out.println("1.hello world");
+System.out.println("2.java world");
+System.out.println("3.java class");
+System.out.println("4.java classes");
+System.out.println("5.software program");
+System.out.println("6.software classes");
+int a=10;
+System.out.println(a);
+long b=10;
+System.out.println(b);
+float c=2.5f;
+System.out.println(c);
+double d=5.88990;
+System.out.println(d);
+byte e = 23;
+System.out.println(e);
+boolean bo = true;
+System.out.println(bo);
+char vh = 'a';
+System.out.println(vh);
+int x=10,y=11;
+System.out.println(x+""+y);
+long t=10, l=11;
+System.out.println(t + " " +l);
+float t1=10.2f, f=11.3f;
+System.out.println(t1 + " " +f);
+double t2=5.88990,d2=2.3;
+System.out.println(t2+" "+d2);
+byte b1 = 23, b2=24;
+System.out.println(b1+""+b2);
+boolean b3 = true, b4=false;
+System.out.println(b3+""+b4);
+char c1 = 'a',c2='r';
+System.out.println(c1+" "+c2);
+float ft=2.5f;
+int in=20;
+System.out.println(ft+" "+in);
+float fd=2.5f;
+double df=20.100121123;
+System.out.println(fd+" "+df);	
+	
+float fc=2.5f;
+char cf='p';
+System.out.println(fc+" "+cf);
+
+float fb=2.5f;
+boolean bf=true;
+System.out.println(fb+" "+bf);
+double di=2.5890;
+int id=78;
+System.out.println(di+" "+id);
+double dl=2.5123456789;
+long ld=123456789;
+System.out.println(dl+" "+ld);
+double dc=2.5123456789;
+char cd='a';
+System.out.println(dc+" "+cd);
+int il=5;
+long li=123456789;
+System.out.println(il+" "+li);
+int ic=5;
+char ci='d';
+System.out.println(ic+" "+ci);
+int ibo=5;
+boolean boi=true;
+System.out.println(ibo+" "+boi);
+
+int iby=5;
+byte byi=54;
+System.out.println(iby+" "+byi);
+int ine=-5;
+int nei=-54;
+System.out.println(ine+" "+nei);
+double inb=-5.0235;
+double nb=-54.235467;
+System.out.println(ine+" "+nei);
+
+float fn=-5.0235f;
+float nef=-54.235467f;
+System.out.println(ine+" "+nei);
+
+char ca='z';
+//int ai=ca;
+System.out.println(ca+ " "+(int)ca);
+
+int spc=64;
+System.out.println((char)spc);
+
+int ine1=-5;
+int nei1=-54,na=54;
+System.out.println(ine1+" "+nei1+" "+na);
+
+
+char ci1='d', ci2='o',ci3='c';
+System.out.println(ci2+" "+ci1+" "+ci3);
+
+double db1=-54.235467,db11=-54.235467,db2=-54.235467;
+System.out.println(db1+" "+db2+" "+db11);
+
+float fn1=-5.0235f;
+float nef1=-54.235467f,nef2=2.45f;
+System.out.println(fn1+" "+nef1+nef2);
+
+long fn2=-5;
+long nel2=-5,nel=2;
+System.out.println(fn2+" "+nel2+nel);
+
+boolean  fn3=true;
+boolean  neb=false,neb3=true;
+System.out.println(fn3+" "+neb+" "+neb3);
+
+int spc1=64;
+System.out.println(spc1);
+System.out.println(spc1+1);
+
+
+long pc1=647890;
+System.out.println(pc1);
+System.out.println(pc1+1);
+
+
+float sc1=64.2345f;
+System.out.println(sc1);
+System.out.println(sc1+1);
+
+
+double fp2=64.234567;
+System.out.println(sc1);
+System.out.println(sc1+1);
+
+char fep='c';
+System.out.println(fep);
+fep='a';
+System.out.println(fep);
+
+int sc11;
+System.out.println(sc11=0);
+System.out.print(sc11+1);
+
+
+char fa='a', fa1= 'e',fa2='i',fa3='o',fa4='u';
+System.out.println(fa+""+fa1+" "+fa2+""+fa3+" "+fa4);
+
+boolean bo1 = true;
+boolean bo2 = true;
+boolean bo3 = true;
+int i3=-5;
+int ne3=-54;
+
+System.out.println(bo1+"\n"+bo2+"\n"+bo3+"\n"+i3+" "+ne3);
+
+
+	}
+
+}
