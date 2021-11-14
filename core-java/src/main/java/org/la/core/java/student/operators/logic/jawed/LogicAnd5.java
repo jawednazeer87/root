3c65813f2902ac81cdcfc6fefba2715b72594588
jawednazeer87 <jawednazeer87@gmail.com>
2021-11-14 10:44:05 +0800

    more logical statment program

---
+++ core-java/src/main/java/org/la/core/java/student/operators/logic/jawed/LogicAnd5.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.operators.logic.jawed;
+
+public class LogicAnd5 {
+
+	public static void main(String[] args) {
+		
+		int x = 1, y = 2;
+		boolean res = (++x==2) && (y--==1); 
+		
+		System.out.println("res: "+res);
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+	}
+}
