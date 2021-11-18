97bfba79d51178e7db3639811946181d8a663995
AbrahamWang <wang.ibrahim222@gmail.com>
2021-11-17 23:56:50 -0800

    modify Wap49

--- core-java/src/main/java/org/la/core/java/student/datatype/wang/assignment1/Wap49.java
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/assignment1/Wap49.java
@@ -4,6 +4,6 @@ public class Wap49 {
 public static void main(String[] args) {
 //	49. wap declare 5 char variable and assign vowel and print it
 	char cj='a',ck='e',cl='i',cm='o',cn='u';
-	System.out.println(cj+ck+cl+cm+cn);
+	System.out.println(cj+" "+ck+" "+cl+" "+cm+" "+cn);
 }
 }
