65ff0ef183c645a71be9e065d793b024e7677281
ALWaleedRizq <waleedrizq07@gmail.com>
2021-11-18 12:38:31 +0600

    Go Ahead-3

--- core-java/src/main/java/org/la/core/java/student/operators/logic/waleed/LogicOp1.java
+++ core-java/src/main/java/org/la/core/java/student/operators/logic/waleed/LogicOp1.java
@@ -1,13 +1,14 @@
 package org.la.core.java.student.operators.logic.waleed;
 
-public class LogicalOperator1 {
+public class LogicOp1 {
 
 	public static void main(String[] args) {
 		boolean  a = false && true;
 		System.out.println("a: "+a);
-		
 		    a = true || false;
-		System.out.println("a: "+a);
+		    System.out.println("a: "+a);
 	}
 
-}
+	}
+
+
