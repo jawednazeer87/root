4f43ea2ac9ba40355f0ed4330ee6c9e36c4dc5b8
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-11-18 11:24:30 +0530

    16_programs_of_Assignment

---
+++ core-java/src/main/java/org/la/core/java/student/operators_assign/arfaz/WhaWillOu.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.operators_assign.arfaz;
+
+public class WhaWillOu {
+
+	public static void main(String[] args) {
+		int x = 5 +3 *4/ 5 + 4;
+		System.out.println(x);
+
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/operators_assign/arfaz/WillOu.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.operators_assign.arfaz;
+
+public class WillOu {
+
+	public static void main(String[] args) {
+		int i = 19/5; 
+		System.out.println(i);
+		
+//		output:3
+		char a = 65;
+		System.out.println(a);
+//		output:A
+		int s = 'a';
+		System.out.println(s);
+//		output:97
+	}
+
+}
