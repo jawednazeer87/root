26ca020faa66e18eeef888e4e80c980897307ada
sajidmohammed90 <sjmohammed90@gmail.com>
2021-11-18 10:03:18 +0530

    Pre and Post Increment / Decrement Assignment Program completed

--- core-java/src/main/java/org/la/core/java/student/arithmetic/SjMohammed/PrePost01.java
+++ core-java/src/main/java/org/la/core/java/student/arithmetic/SjMohammed/PrePost01.java
@@ -3,8 +3,10 @@ package org.la.core.java.student.arithmetic.SjMohammed;
 public class PrePost01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a =10, b=++a;
+		System.out.println("a = "+ a+" b = "+ b);
+		
 	}
 
 }
---
+++ core-java/src/main/java/org/la/core/java/student/arithmetic/SjMohammed/PrePost13.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.arithmetic.SjMohammed;
+
+public class PrePost13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b = a++ / 3;
+		System.out.println("A "+a+" B "+b);
+	}
+
+}
