ee934237c43e2fdd0fd8281323c92e8a3cabfcb0
ALWaleedRizq <waleedrizq07@gmail.com>
2021-11-18 12:10:39 +0600

    Go Ahead-1

---
+++ core-java/src/main/java/org/la/core/java/student/operators/logic/waleed/OrLogic1.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.operators.logic.waleed;
+
+public class OrLogic1 {
+
+	public static void main(String[] args) {
+		boolean a = true || false;
+		System.out.println("a: "+a);
+		a = true || true;
+		System.out.println("a: "+a);
+		a = false || false;
+		System.out.println("a: "+a);
+		a = false || true;
+		System.out.println("a: "+a);
+
+	}
+
+}
