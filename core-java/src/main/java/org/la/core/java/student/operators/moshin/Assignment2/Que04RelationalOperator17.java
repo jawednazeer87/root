a41739e79bd7c89a96e55923222144f0fb23adf1
moshin <moshin.ma715@gmail.com>
2021-11-21 22:44:12 +0530

    Assignment2

---
+++ core-java/src/main/java/org/la/core/java/student/operators/moshin/Assignment2/Que04RelationalOperator17.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.operators.moshin.Assignment2;
+
+public class Que04RelationalOperator17 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		int a = 1, b = 2, c = -3;
+		int s = a++ + b-- * c++;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(s);
+	}
+
+}
