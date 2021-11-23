27f5d439c490d7054f8b2a4992dbd7039ed66c64
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-11-23 12:00:37 +0530

    Operators_Assign

---
+++ core-java/src/main/java/org/la/core/java/student/operators_assign/arfaz/CharVar.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.operators_assign.arfaz;
+
+public class CharVar {
+
+	public static void main(String[] args) {
+		char a= 'A';
+		char b= 'R';
+        System.out.println(a+b);	
+       
+        }
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/operators_assign/arfaz/Eightee.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.operators_assign.arfaz;
+
+public class Eightee {
+
+	public static void main(String[] args) {
+		int num = 69;
+        int days = num % 30;
+        int month = num / 30;
+        System.out.println(month + " Month and " + days + " days");	}
+
+}
