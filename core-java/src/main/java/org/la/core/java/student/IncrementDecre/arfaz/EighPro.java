d383aca7dc821e1326c75ad0a35697c4d46e24fe
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-11-24 11:29:16 +0530

    Increment and Decreement by Arfaz

---
+++ core-java/src/main/java/org/la/core/java/student/IncrementDecre/arfaz/EighPro.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.IncrementDecre.arfaz;
+
+public class EighPro {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = ++a;
+        	
+        	
+        	System.out.println(a+ " "+b);
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/IncrementDecre/arfaz/FortPro.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.IncrementDecre.arfaz;
+
+public class FortPro {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = -2;
+		int z = ++x * y++;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+	}
+
+}
