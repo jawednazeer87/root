1e75f185af9f8dbf6afac6ec659ae8f12baae1cf
moideen <moideen@Admin>
2021-11-24 17:09:03 +0530

    printed two boo variable

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/twobooleanvariable.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class twobooleanvariable {
+
+	public static void main(String[] args) {
+		
+		boolean i=true&&false;
+		boolean j=false||true;
+
+		System.out.println(i);
+		System.out.println(j);
+		
+	}
+
+}
