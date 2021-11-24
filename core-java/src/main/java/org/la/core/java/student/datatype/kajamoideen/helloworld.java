66015e7d065bd3ad5580c641c409237d339c94ee
86014355 <kajamoideenkj@gmail.com>
2021-11-24 13:36:53 +0530

    test

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/helloworld.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.datatype.kajamoideen;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello world");
+		
+	}
+
+}
