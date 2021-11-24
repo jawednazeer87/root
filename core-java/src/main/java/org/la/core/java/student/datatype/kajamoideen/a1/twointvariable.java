a615627320573d85a7d255bff2b305f985a38ebe
moideen <moideen@Admin>
2021-11-24 16:48:31 +0530

    printed two int variable

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/twointvariable.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class twointvariable {
+
+	public static void main(String[] args) {
+		
+		int i=10;
+		int x=15;
+		int sum = i+x;
+
+		System.out.println(i+" "+x+" "+sum);
+		
+	}
+
+}
