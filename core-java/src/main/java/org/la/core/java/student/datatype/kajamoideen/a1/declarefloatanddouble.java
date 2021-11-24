6d9839485793a896bc3cccb96e4af6281af424e6
moideen <moideen@Admin>
2021-11-25 00:06:30 +0530

    printed float and double

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarefloatanddouble.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarefloatanddouble {
+
+	public static void main(String[] args) {
+		
+		 double i=10.111;
+		 float j=1.11f;
+
+		System.out.println("This is double and float Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
