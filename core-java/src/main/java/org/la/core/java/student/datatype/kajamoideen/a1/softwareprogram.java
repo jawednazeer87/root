25d65ba4609f7dcb6834890a603346281447b845
moideen <moideen@Admin>
2021-11-24 15:51:25 +0530

    printed software program

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/softwareprogram.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class softwareprogram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Software Program");
+		
+	}
+
+}
