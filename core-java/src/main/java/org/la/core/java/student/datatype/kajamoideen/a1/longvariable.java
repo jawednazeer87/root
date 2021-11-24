722b016758e661e497d79e1adba5d9208f1a07f4
moideen <moideen@Admin>
2021-11-24 16:02:02 +0530

    printed long variable

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/longvariable.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class longvariable {
+
+	public static void main(String[] args) {
+		
+		long i=100000000000L;
+
+		System.out.println("This is long Variable"+" "+i);
+		
+	}
+
+}
