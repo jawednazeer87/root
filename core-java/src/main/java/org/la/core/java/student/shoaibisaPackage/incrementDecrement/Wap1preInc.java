914477efaa42e0453691cc28575c0b22cabd98bd
shoaib <shoaibisa1@gmail.com>
2021-11-24 17:42:55 +0530

    pre inc

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/incrementDecrement/Wap1preInc.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.shoaibisaPackage.incrementDecrement;
+
+public class Wap1preInc {
+
+	public static void main(String[] args) {
+		int a=4, b;
+		b = ++a;
+		System.out.println("a = "+a+", b= "+b);
+	}
+
+}
