07f4353663b1e8890d677ba894e191e979697a5d
moideen <moideen@Admin>
2021-11-24 17:02:03 +0530

    printed two byte variable

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/twobytevariable.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class twobytevariable {
+
+	public static void main(String[] args) {
+		
+		 byte i=10;
+
+		System.out.println("This is Byte Variable"+" "+i);
+		
+	}
+	
+
+}
