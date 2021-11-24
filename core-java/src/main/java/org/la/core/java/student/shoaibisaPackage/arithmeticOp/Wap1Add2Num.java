c79b8365f9d7300188bae12ac14d0ed107772791
shoaib <shoaibisa1@gmail.com>
2021-11-24 16:24:54 +0530

    arithmetic

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/arithmeticOp/Wap1Add2Num.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.shoaibisaPackage.arithmeticOp;
+
+import java.util.*;
+
+public class Wap1Add2Num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1, num2, sol;
+		Scanner sc = new Scanner(System.in);    //System.in is a standard input stream
+		System.out.println("Enter two number ");
+		num1 = sc.nextInt();
+		num2 = sc.nextInt();
+		sol = num1 + num2;
+		System.out.println(" "+num1+" + " + num2+" = "+sol);
+	}
+
+}
