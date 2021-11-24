bf4d822cb1b3511dae00e296560daa237a50d5c1
moideen <moideen@Admin>
2021-11-24 23:43:57 +0530

    printed float and int

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarefloatandint.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarefloatandint {
+
+	public static void main(String[] args) {
+		
+		 int i=10;
+		 float j=1.11f;
+
+		System.out.println("This is int and float Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
