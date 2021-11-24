413e8172ecc55b9ab7d840dcef0872133b711858
moideen <moideen@Admin>
2021-11-24 16:54:44 +0530

    printed tow long variable

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/twolongvariable.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class twolongvariable {
+
+	public static void main(String[] args) {
+		
+		long i=1192929;
+	    long x=112111;
+		long sum = i+x;
+
+		System.out.println(i+", "+x+" ,"+sum);
+		
+	}
+
+}
