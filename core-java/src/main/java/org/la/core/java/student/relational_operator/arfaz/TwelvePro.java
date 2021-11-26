2e36f770413958a81c5f4862625cb8547da19fd7
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-11-26 10:13:54 +0530

    Relational_Part_2

---
+++ core-java/src/main/java/org/la/core/java/student/relational_operator/arfaz/TwelvePro.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.relational_operator.arfaz;
+
+public class TwelvePro {
+
+	public static void main(String[] args) {
+	
+		int x = -5;
+		System.out.println("result: "+(x==-5 && ++x>-5)); 
+
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/relational_operator/arfaz/TwenteonPro.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.relational_operator.arfaz;
+
+public class TwenteonPro {
+
+	public static void main(String[] args) {
+	
+		
+		boolean x = false, y = true;
+		boolean r = x && y;
+		
+		System.out.println(r);
+		
+	
+	}
+
+}
