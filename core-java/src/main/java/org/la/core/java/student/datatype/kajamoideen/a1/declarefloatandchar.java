1138c98bc219aea1969f74062ee64b5b630400c9
moideen <moideen@Admin>
2021-11-30 23:54:28 +0530

    Declared float and char variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarefloatandchar.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarefloatandchar {
+
+	public static void main(String[] args) {
+		
+		 char i='B';
+		 float j=1.11f;
+
+		System.out.println("This is float and char Variable "+""+j+ ","+"'"+i+"'");
+		
+	}
+	
+
+}
