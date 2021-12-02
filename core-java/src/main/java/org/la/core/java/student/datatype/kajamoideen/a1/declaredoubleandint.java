54ce8e50de8003a3dcba691685a073a5a3574b48
moideen <moideen@Admin>
2021-12-02 16:58:10 +0530

    printed Double and int variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declaredoubleandint.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declaredoubleandint {
+
+	public static void main(String[] args) {
+		
+		 double i=12.22;
+		 int j=1222;
+
+		System.out.println("This is Double and int Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
