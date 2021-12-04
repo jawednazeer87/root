e4c1a71b61c1d8c71c5beff1438ccba717ead14d
moideen <moideen@Admin>
2021-12-04 15:59:49 +0530

    printed three float variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarethreefloat.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarethreefloat {
+
+	public static void main(String[] args) {
+		float a=12.1f,b=1.1f,c=43.2f;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+	
+
+}
