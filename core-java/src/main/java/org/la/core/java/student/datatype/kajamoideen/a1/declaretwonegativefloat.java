ab391d5a26f2c0cfaebe7732fe9f7d45af3ca10b
moideen <moideen@Admin>
2021-12-04 15:35:51 +0530

    printed two negative float variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declaretwonegativefloat.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declaretwonegativefloat {
+
+	public static void main(String[] args) {
+		
+		 float i=-1.2f,j=-2.1f;
+		 
+
+		System.out.println("This is two negative float Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
