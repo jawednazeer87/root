4116c48ebedffb21cd6324afc8d571b12e6b285b
moideen <moideen@Admin>
2021-12-04 15:54:24 +0530

    printed three int variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarethreeint.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarethreeint {
+
+	public static void main(String[] args) {
+		int a=1,b=2,c=3;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+	
+
+}
