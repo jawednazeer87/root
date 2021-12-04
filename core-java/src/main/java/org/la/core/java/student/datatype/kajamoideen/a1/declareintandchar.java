5bdfa1fd54c78eecab3b1badaf223d6f0113b109
moideen <moideen@Admin>
2021-12-04 15:21:31 +0530

    printed int and char variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declareintandchar.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declareintandchar {
+
+	public static void main(String[] args) {
+		
+		 int i=122;
+		 char j='w';
+
+		System.out.println("This is int and char Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
