62a5468bbe69822353f4768badf694f68f330d7e
moideen <moideen@Admin>
2021-12-04 15:23:08 +0530

    printed int and bool variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declareintandboolean.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declareintandboolean {
+
+	public static void main(String[] args) {
+		
+		 int i=122;
+		 boolean j=false;
+
+		System.out.println("This is int and boolean Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
