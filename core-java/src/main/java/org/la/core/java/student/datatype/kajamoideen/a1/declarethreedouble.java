a37b18cb6dbfba5e7e6cd7ea30934f419b66c828
moideen <moideen@Admin>
2021-12-04 15:58:32 +0530

    printed three double variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarethreedouble.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarethreedouble {
+
+	public static void main(String[] args) {
+		double a=12.12,b=1.12,c=43.22;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+	
+
+}
