20bc61bdb87745523771e7ba3b5a2ad650f373cb
moideen <moideen@Admin>
2021-12-04 17:06:08 +0530

    printed two bool, three int and one long

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/assignmnetonefinal.java
@@ -0,0 +1,21 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class assignmnetonefinal  {
+
+	public static void main(String[] args) {
+		boolean cs=true,ct=false;
+		int co=666,cp=777,cq=888;
+		long crr=123345364234L;
+		System.out.println(cs);
+		System.out.println(ct);
+		System.out.println(co); 	
+		System.out.println(cp);
+		System.out.println(cq);
+		System.out.println(crr);
+		System.out.println(crr+""+cq+""+cp+""+co+"");
+		System.out.println(cs);
+		System.out.println(ct);
+	}
+	
+
+}
