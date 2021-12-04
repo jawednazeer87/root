97b8509bf406a7cc4a0a50de8bf4d8f5d496a8f5
moideen <moideen@Admin>
2021-12-04 15:26:54 +0530

    printed int and byte variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declareintandbyte.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declareintandbyte {
+
+	public static void main(String[] args) {
+		
+		 int i=1;
+		 byte j=127;
+
+		System.out.println("This is int and byte Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
