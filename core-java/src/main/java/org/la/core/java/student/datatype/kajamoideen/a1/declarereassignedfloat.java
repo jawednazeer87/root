0e75d6549b7519ab3c66ba406a8c44a0164aee2b
moideen <moideen@Admin>
2021-12-04 16:08:57 +0530

    printed reassigned float variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarereassignedfloat.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarereassignedfloat {
+
+	public static void main(String[] args) {
+		float i=1.1f;
+		System.out.println(i);
+		i=1.2f;
+		System.out.println(i);
+		
+	}
+	
+
+}
