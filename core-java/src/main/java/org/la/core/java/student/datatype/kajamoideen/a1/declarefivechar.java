986bad0fcae321aec928f03465ca8eb94fe08ee1
moideen <moideen@Admin>
2021-12-04 16:18:11 +0530

    printed five char variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarefivechar.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarefivechar  {
+
+	public static void main(String[] args) {
+		char cj='a',ck='e',cl='i',cm='o',cn='u';
+		System.out.println(cj);
+		System.out.println(ck);
+		System.out.println(cl);
+		System.out.println(cm);
+		System.out.println(cn);
+		
+	}
+	
+
+}
