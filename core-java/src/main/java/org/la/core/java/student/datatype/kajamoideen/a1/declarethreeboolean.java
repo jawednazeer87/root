3c877811981e189cd22f9824c13604350e990ce0
moideen <moideen@Admin>
2021-12-04 16:02:37 +0530

    printed three boolean variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarethreeboolean.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarethreeboolean {
+
+	public static void main(String[] args) {
+		boolean a=true,b=false,c=true;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+	}
+	
+
+}
