62c8f9fb189b5dbabc9aff329b10926da5f69166
moideen <moideen@Admin>
2021-12-04 15:19:39 +0530

    printed int and long variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declareintandlong.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declareintandlong {
+
+	public static void main(String[] args) {
+		
+		 int i=122;
+		 long j=102910192029l;
+
+		System.out.println("This is int and long Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
