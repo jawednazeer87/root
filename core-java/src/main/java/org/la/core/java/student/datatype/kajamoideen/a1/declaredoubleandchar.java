06044a3bb778e4b87fad7f4b69782e810db6d61a
moideen <moideen@Admin>
2021-12-04 13:48:03 +0530

    printed double and char variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declaredoubleandchar.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declaredoubleandchar {
+
+	public static void main(String[] args) {
+		
+		 double i=12.22;
+		 long j='B';
+
+		System.out.println("This is Double and char Variable "+""+i+" ,"+j);
+		
+	}
+	
+
+}
