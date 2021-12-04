ed6ed5cf0d677546edf3637d22284502d48db4b8
moideen <moideen@Admin>
2021-12-04 16:11:47 +0530

    printed reassigned char variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarereassignedchar.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarereassignedchar {
+
+	public static void main(String[] args) {
+		char i='K';
+		System.out.println(i);
+		i='J';
+		System.out.println(i);
+		
+	}
+	
+
+}
