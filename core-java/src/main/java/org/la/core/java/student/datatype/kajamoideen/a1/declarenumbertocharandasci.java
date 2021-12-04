9a2712b8ca7fbb5723eb805ef126ecdfedd79e3f
moideen <moideen@Admin>
2021-12-04 15:50:38 +0530

    printed number as char and ascii value

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declarenumbertocharandasci.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+
+public class declarenumbertocharandasci {
+
+	public static void main(String[] args) {
+		
+		char num='1';
+		int num1=(int)num;
+		 
+
+		System.out.println("Given char as int:"+num);
+		System.out.println("Given char as ascii:"+num1);
+		
+	}
+	
+
+}
