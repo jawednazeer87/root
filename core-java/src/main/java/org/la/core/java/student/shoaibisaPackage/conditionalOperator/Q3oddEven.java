1736407ad0376798f2a5566ed9aa7f61adc4c073
shoaib <shoaibisa1@gmail.com>
2021-12-04 11:41:08 +0530

    Odd Even

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/conditionalOperator/Q3oddEven.java
@@ -0,0 +1,21 @@
+package org.la.core.java.student.shoaibisaPackage.conditionalOperator;
+
+import java.util.Scanner;
+
+public class Q3oddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter a number ");
+	        int num = sc.nextInt();
+	
+	        
+	        if(num%2==0) {
+	        	 System.out.println("Even Number ");
+	        } else {
+	        	System.out.println("Odd Number ");
+	        }
+	}
+
+}
