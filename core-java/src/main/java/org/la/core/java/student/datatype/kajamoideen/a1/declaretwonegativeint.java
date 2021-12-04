ea912fa239c45a3b2132de1f16e2d35bd0494d0c
moideen <moideen@Admin>
2021-12-04 15:31:50 +0530

    printed two negative int variables

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a1/declaretwonegativeint.java
@@ -0,0 +1,9 @@
+package org.la.core.java.student.datatype.kajamoideen.a1;
+public class declaretwonegativeint{
+	public static void main(String[]args) {
+		int i=-1,j=-3;
+		System.out.println("This is two negative int Variable "+i+","+j);
+		
+		
+	}
+}
