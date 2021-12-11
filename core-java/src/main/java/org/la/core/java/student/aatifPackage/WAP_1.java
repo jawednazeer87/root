5a7db26b5d50f6265af04a31668ac2a98ac43f52
AATIF773 <aatifnayeem3@gmail.com>
2021-12-11 12:19:18 +0530

    1st commit

---
+++ core-java/src/main/java/org/la/core/java/student/aatifPackage/WAP_1.java
@@ -0,0 +1,10 @@
+package org.la.core.java.student.aatifPackage;
+
+public class WAP_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
