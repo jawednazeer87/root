3cecc39d6dcf26f3212d28f9592dd1ba423a5974
AATIF773 <aatifnayeem3@gmail.com>
2021-12-11 12:28:44 +0530

    arithmetic-1

---
+++ core-java/src/main/java/org/la/core/java/student/aatifPackage/arithmetic/WAP_1.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.aatifPackage.arithmetic;
+
+public class WAP_1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2, b = 4, c;
+		
+		c = a + b;
+		System.out.println("1st number : " + a);
+		System.out.println("2nd number : " + b);
+		System.out.println("Total Sum : " + c);
+	}
+}
