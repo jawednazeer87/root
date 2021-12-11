42724604ab427b15a4b7bb1c8b5d76bc7298190f
mudassir <mudassirakhter420@gmail.com>
2021-12-11 11:25:57 +0530

    my first program

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/dataType/Wap1.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.mudassirPackage.dataType;
+
+public class Wap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
