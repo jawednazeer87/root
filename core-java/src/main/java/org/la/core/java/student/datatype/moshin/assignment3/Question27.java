dec4600d9bfba6c0b395284852a3fd98b04576bc
moshin <moshin.ma715@gmail.com>
2021-12-12 23:26:04 +0530

    Assignment 3

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/moshin/assignment3/Question27.java
@@ -0,0 +1,28 @@
+package org.la.core.java.student.datatype.moshin.assignment3;
+
+public class Question27 {
+
+	public static void main(String[] args) {
+		
+		float f = 71.20f;
+        double d = 71.20;
+        int i = 75;
+        if( f == d )
+        {
+            if( f == i )
+            {
+                System.out.println("f, d and i are equal");
+            }
+            else
+            {
+                System.out.println("f, d are equal but i is not equal");
+            }
+        }
+        else
+        {
+            System.out.println("f and d are not equal");
+        }
+    
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/datatype/moshin/assignment3/Question36.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.moshin.assignment3;
+
+public class Question36 {
+
+	public static void main(String[] args) {
+		
+		int a = 30;
+        if (a < 100) {
+            System.out.println(a + " is less than 100");
+        } else {
+            System.out.println(a + " is not less than 100");
+        }
+        
+	}
+
+}
