020f3c58fec150cc15631b008b9ee7e38c4ad166
moshin <moshin.ma715@gmail.com>
2021-12-13 00:16:45 +0530

    Assignment 3

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/moshin/assignment3/Question42.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.moshin.assignment3;
+
+public class Question42 {
+
+	public static void main(String[] args) {
+		
+		int a = 58;
+        if (a > -10 && a < 10) {
+            System.out.println("single");
+        } else {
+            System.out.println("not single");
+        }
+        
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/datatype/moshin/assignment3/Question48.java
@@ -0,0 +1,24 @@
+package org.la.core.java.student.datatype.moshin.assignment3;
+
+public class Question48 {
+
+	public static void main(String[] args) {
+		
+		int a = 451, b = 370, c = 111;
+        if (a < b) {
+            if (a < c) {
+                System.out.println(a + " is smallest number");
+            } else {
+                System.out.println(c + " is smallest number");
+            }
+        } else {//a>b
+            if (c > b) {
+                System.out.println(b + " is smallest number");
+            } else {
+                System.out.println(c + " is smallest number");
+            }
+        }
+        
+	}
+
+}
