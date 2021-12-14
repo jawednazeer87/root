7b10bb717c4af4641370595b09ea817fc5839ec6
AATIF773 <aatifnayeem3@gmail.com>
2021-12-14 22:19:46 +0530

    Qno 14 to 20

---
+++ core-java/src/main/java/org/la/core/java/student/aatifPackage/WAP_14_20.java
@@ -0,0 +1,28 @@
+package org.la.core.java.student.aatifPackage;
+
+public class WAP_14_20 {
+
+	public static void main(String[] args) {
+		
+		//Q.no 14 to 20
+		
+	int i_num1=500000, i_num2=20000;
+	long l_num1=1500000000l, l_num2=80080000l;
+	float f_num1=5.63f, f_num2=8.9385f;
+	double d_num1=9.99999d, d_num2=5.6698754d;
+	byte b_num1=-100, b_num2=122;
+	boolean isAatif=true, isnotAatif=false;
+	char java_grade='A', python_grade='B';
+		
+		System.out.println("Two Intiger value : "+ i_num1 + " , " + i_num2);
+		System.out.println("Two Long value : " + l_num1 + " , " + l_num2);
+		System.out.println("Two Float value : " + f_num1 + " , " + f_num2);
+		System.out.println("Two Double value : " + d_num1 + " , " + d_num2);
+		System.out.println("Two Byte value : " + b_num1 + " , " + b_num2);
+		System.out.println("Two Boolean value : " + isAatif + " , " + isnotAatif);
+		System.out.println("Two Character value : " + java_grade + " , " + python_grade);
+		
+
+	}
+
+}
