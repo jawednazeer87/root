9af2b4b0383657cc490152d63d2b318b0ec77eae
mudassir <mudassirakhter420@gmail.com>
2021-12-15 18:20:50 +0530

    first program

--- core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap2.java
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap2.java
@@ -5,8 +5,7 @@ public class Wap2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Java!");
-		//hello world
-		//hello java
+		
 
 	}
 
