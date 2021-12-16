5eb46911e3f6d90b1bd54413c6a187d5adbdbd7f
mudassir <mudassirakhter420@gmail.com>
2021-12-16 12:17:10 +0530

    question7

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/wap7.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class wap7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 2;
+		System.out.println(a);
+
+	}
+
+}
