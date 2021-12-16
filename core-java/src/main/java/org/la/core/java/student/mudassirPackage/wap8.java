28457181c8ffda3fda5e72fa260b7967c243ad96
mudassir <mudassirakhter420@gmail.com>
2021-12-16 12:18:32 +0530

    question8

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/wap8.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class wap8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long a=300;
+		System.out.println(a);
+
+	}
+
+}
