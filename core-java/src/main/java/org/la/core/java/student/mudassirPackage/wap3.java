fbd7766248cc9c0af82cf2fa96d6d797d7d028d9
mudassir <mudassirakhter420@gmail.com>
2021-12-16 12:08:19 +0530

    question3 java class

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/wap3.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class wap3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java class");
+
+	}
+
+}
