9b5ebd7c666bd2380ae298fdde57aa5f091b869b
mudassir <mudassirakhter420@gmail.com>
2021-12-16 12:13:37 +0530

    question6

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/wap6.java
@@ -0,0 +1,10 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class wap6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         System.out.println("software classes");
+	}
+
+}
