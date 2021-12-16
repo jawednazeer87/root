e58cc23157455897b8c5781137dd9f48a6d26079
mudassir <mudassirakhter420@gmail.com>
2021-12-16 12:12:08 +0530

    question5

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/wap5.java
@@ -0,0 +1,9 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class wap5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("software program");
+
+	}
+}
