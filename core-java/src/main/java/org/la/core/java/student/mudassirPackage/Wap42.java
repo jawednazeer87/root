ced60822db5549b0eef89b2e9bc3d592bb25674c
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:17:44 +0530

    wap to declare 3 boolean variables and assign any value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap42.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap42 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a=true,b=false,c=false;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
