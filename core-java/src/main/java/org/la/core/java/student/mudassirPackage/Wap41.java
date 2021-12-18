a318c8a9704b5e5ca876687b7bd38a91402664a6
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:16:40 +0530

    wap to declare 3 long variables and assign any value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap41.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long a=190,b=240,c=533;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
