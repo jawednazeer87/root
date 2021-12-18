87f4c4b7705431a6c23ea39703ef25020a74fe8e
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:34:44 +0530

    wap declare float and int variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap21.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float x=4.8f; int y=8;
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
