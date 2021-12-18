aa6765d4dc8acec622b7362315427006ae391684
mudassir <mudassirakhter420@gmail.com>
2021-12-18 12:50:31 +0530

    double in java

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap10.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 95.96;
+		System.out.println(a);
+	}
+
+}
