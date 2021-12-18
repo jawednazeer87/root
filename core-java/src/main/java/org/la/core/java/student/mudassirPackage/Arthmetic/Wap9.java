8a6ad513134de0a108047891e06e24c860547290
mudassir <mudassirakhter420@gmail.com>
2021-12-18 17:49:23 +0530

    wap to find average of 3 numbers?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap9.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+      float a = 10, b = 12, c = 14, avg;
+		
+		avg =  (a + b + c)/3;
+		System.out.println("Three numbers are : " + a + " , " + b + " , " + c);
+		System.out.println("Average : " + avg);
+
+	}
+
+}
