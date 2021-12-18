2e4c463e2e25c074ea03fd795e50206d3f48b5cc
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:57:58 +0530

    wap to print 2 negative double

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap33.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap33 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a=-1.90, b=-5.2;
+		System.out.println("A: "+a);
+		System.out.println("B: "+b);
+
+	}
+
+}
