7e68162dbc17ca189c38895d71128dc4f8543a53
moideen <moideen@Admin>
2021-12-18 18:05:48 +0530

    Subtracted two Numbers

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_3.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+  
+   public class ArithmeticOperation_3 {
+	   
+	   public static void main(String args[]) 
+	   {
+		   int a = 7, b = 2;
+		   
+		   int sub = a - b;
+		   
+		   System.out.println(sub);  
+		   
+	   }
+	   
+	   
+   }
