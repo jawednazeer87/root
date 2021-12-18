ed4138a4c754d588ff186127cf656b6ed935e130
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:11:01 +0530

    wap to declare 3 double variables and assign any value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap39.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap39 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a=50.00,b=60.2,c=70.25;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
