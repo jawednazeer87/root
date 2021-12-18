d843f3e07f2fe56aa150779b58fa3f0361bab37c
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:47:55 +0530

    wap declare double and char variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap27.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x=72.00; char y='A';
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
