c14a297cdecb79909a869661c9ff62ef172900c4
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:49:22 +0530

    wap declare int and long variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap28.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=70; long y=160;
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
