5b14c7b293c3c3e24229b0c912d979396d1e0b5a
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:48:38 +0530

    wap declare int and print its value then change its value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap48.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap48 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=50;
+		System.out.println("A: "+a);
+		a=10;
+		System.out.println("A: "+a);
+
+	}
+
+}
