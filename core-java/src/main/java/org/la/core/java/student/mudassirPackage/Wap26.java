1148f3b9fc3b9cf020d58640e96e361c535f3a7b
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:46:04 +0530

    wap declare double and long variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap26.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap26 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x=72.00; long y=100;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
