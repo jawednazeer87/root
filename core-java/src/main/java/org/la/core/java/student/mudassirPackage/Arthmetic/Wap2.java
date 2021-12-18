236428af01ab99932aef265ac2cf524359ec4df0
mudassir <mudassirakhter420@gmail.com>
2021-12-18 15:03:35 +0530

    wap to add three numbers?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap2.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 50;
+		int num2 = 60;
+		int num3 = 10;
+		int c = num1 + num2 + num3;
+		
+		System.out.println("Sum of "+ num1 +" + " + num2 +" + " + num3 +" is " + c);
+		
+
+	}
+
+}
