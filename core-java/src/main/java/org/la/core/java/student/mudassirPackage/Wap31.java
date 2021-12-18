433cdb138fa11094ff51842386144f735144f4df
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:54:02 +0530

    wap declare int and byte variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap31.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=70; byte y=1;
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
