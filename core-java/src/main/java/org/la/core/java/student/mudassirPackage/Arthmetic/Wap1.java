5b4a9f6424456ef3e26506abf4465d2a532a20dd
mudassir <mudassirakhter420@gmail.com>
2021-12-18 15:00:00 +0530

    wap to add two numbers?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap1.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 40;
+		int num2 = 60;
+		int c = num1 + num2;
+		
+		System.out.println("Sum of "+ num1 +" + " + num2 +" is " + (num1) + (num2));
+		System.out.println("Printing sum using variable : " + c);
+		
+
+	}
+
+}
