37c3f13cb24c2a1b078ae99d758ab68f2aa91800
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:59:26 +0530

    wap to print 2 negative float

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap34.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a=-1.28f, b=-90.0f;
+		System.out.println("A: "+a);
+		System.out.println("B: "+b);
+
+	}
+
+}
