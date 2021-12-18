eded3f784459bd21e7e0d7bf4e38485fcc9f8ba8
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:15:14 +0530

    wap to declare 3 float variables and assign any value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap40.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a=5.0f,b=2.0f,c=3.0f;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
