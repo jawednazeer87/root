c966e4a2a8b984c6b842ebcefcb42144c8b63089
mudassir <mudassirakhter420@gmail.com>
2021-12-18 15:57:20 +0530

    wap to find percentage?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap8.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+       float total_no = 1475, obtained_no= 1249, per;
+		
+		per = (obtained_no/total_no) * 100;
+		System.out.println("Number obtained : " + obtained_no);
+		System.out.println("Total number : " + total_no);
+		System.out.println("Percentage : " + per);
+		
+
+	}
+
+}
