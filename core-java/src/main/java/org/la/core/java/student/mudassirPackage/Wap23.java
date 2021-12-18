f1a718eb0d2b965332b3a375c64194dc710406d6
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:39:49 +0530

    wap declare float and char variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap23.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap23 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float x=5.8f; char y='A';
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
