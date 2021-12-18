208b3448704f51a5fa50c29a61f5ded374ef60d0
mudassir <mudassirakhter420@gmail.com>
2021-12-18 15:46:22 +0530

    wap to multiply two numbers?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap5.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 8, b = 4, c;
+		
+		c = a * b;
+		System.out.println("1st number : " + a);
+		System.out.println("2nd number : " + b);
+		System.out.println("Total Sum : " + c);
+		
+	}
+
+}
