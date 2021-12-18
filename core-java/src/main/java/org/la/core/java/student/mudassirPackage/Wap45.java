362f1beb198c9616c55696e9166953b8f4f97581
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:22:27 +0530

    wap declare float variable and print its value then change its value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap45.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float a=60.5f;
+		System.out.println("A: "+a);
+		a=30f;
+		System.out.println("A: "+a);
+
+	}
+
+}
