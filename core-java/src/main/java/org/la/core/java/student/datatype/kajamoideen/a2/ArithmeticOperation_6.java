9c31489e226d8039f2a0e1ae6add7aa13b9ba8f9
moideen <moideen@Admin>
2021-12-18 18:29:35 +0530

    Multiplied Three Numbers

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_6.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+  
+   public class ArithmeticOperation_6 {
+	   
+	   public static void main(String args[]) 
+	   {
+		   int a = 7, b = 2 , c = 4;
+		   
+		   int Mul = a * b * c;
+		   
+		   System.out.println(Mul);  
+		   
+	   }
+	   
+	   
+   }
