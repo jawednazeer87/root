4b59d2c57698f7ca48262b6fb68ca6a6fd9a3eab
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:03:14 +0530

    wap to print number as char and its ascii value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap35.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap35 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char num = '1';
+		System.out.println("Number as Char: " +num);
+		System.out.println("ASCII Value" +(int)num);
+
+	}
+
+}
