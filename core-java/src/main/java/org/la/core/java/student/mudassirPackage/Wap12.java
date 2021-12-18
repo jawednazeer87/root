72577d519a2ca02557809382ac78da3d63eda46a
mudassir <mudassirakhter420@gmail.com>
2021-12-18 12:56:22 +0530

    quest 12 boolean

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap12.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a= false;
+		System.out.println(a);
+
+	}
+
+}
