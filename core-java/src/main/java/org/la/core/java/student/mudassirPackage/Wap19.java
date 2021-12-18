38c536c14f483d55f78c9db81f5f5e975e165ebb
mudassir <mudassirakhter420@gmail.com>
2021-12-18 13:29:50 +0530

    wap declare 2 boolean variables

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap19.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean x=true, y=false;
+		System.out.println(x);
+		System.out.println(y);
+
+	}
+
+}
