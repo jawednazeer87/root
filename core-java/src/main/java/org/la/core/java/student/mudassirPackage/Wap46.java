0dca9969cc2598f3c37370c93f4c4ef97b5859a1
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:45:11 +0530

    wap declare double variable and print its value then change its value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap46.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap46 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double a=108.55;
+		System.out.println("A: "+a);
+		a=555.155;
+		System.out.println("A: "+a);
+
+	}
+
+}
