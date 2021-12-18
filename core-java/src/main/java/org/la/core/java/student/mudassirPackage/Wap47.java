d463b725dc7a6e0ba600bd34847daceb5e3377fa
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:47:15 +0530

    wap declare char variable and print its value then change its value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap47.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap47 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char a='B';
+		System.out.println("A: "+a);
+		a='A';
+		System.out.println("A: "+a);
+
+	}
+
+}
