4cd5b0aae5f768cf479f01a2f8a0136a7ddb979b
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:05:38 +0530

    wap to declare int spc = 32; and print its char value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap36.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap36 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num = 90;
+		System.out.println("Number as Char: " + (char)num);
+		System.out.println("int Value: " +num);
+
+	}
+
+}
