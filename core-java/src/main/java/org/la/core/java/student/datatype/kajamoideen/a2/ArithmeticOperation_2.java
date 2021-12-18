5897a00d6828a0b6a550c515a17f4032dc9995f0
moideen <moideen@Admin>
2021-12-18 17:43:32 +0530

    added three numbers

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_2.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//1.	wap to add two numbers?
+		int a = 1, b = 2, c = 7;
+		int sum = a + b + c;
+		System.out.println(sum);
+	}
+
+}
