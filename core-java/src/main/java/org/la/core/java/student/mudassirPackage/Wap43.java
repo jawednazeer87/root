4f9ab0a2ed6bc99882f1b5ff4092317cc56e5554
mudassir <mudassirakhter420@gmail.com>
2021-12-18 14:19:19 +0530

    wap declare int variable and print its value then change its value

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Wap43.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage;
+
+public class Wap43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=8;
+		System.out.println("A: "+a);
+		a=10;
+		System.out.println("A: "+a);
+
+	}
+
+}
