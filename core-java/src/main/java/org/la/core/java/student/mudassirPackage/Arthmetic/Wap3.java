90c53162044de73759f66a96aa0bba133e33548d
mudassir <mudassirakhter420@gmail.com>
2021-12-18 15:31:55 +0530

    wap to subtract two numbers?

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap3.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        int a = 5, b = 3, c;
+		
+		c = a - b;
+		System.out.println("first number : " + a);
+		System.out.println("second number : " + b);
+		System.out.println("Total Substract : " + c);
+
+	}
+
+}
