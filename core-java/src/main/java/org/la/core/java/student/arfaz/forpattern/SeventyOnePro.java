2367f2db9f864642f3c59376a09c8bc70d623719
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-12-20 11:13:15 +0530

    Assignment4_seventyone

--- core-java/src/main/java/org/la/core/java/student/arfaz/forpattern/SeventyOnePro.java
+++ core-java/src/main/java/org/la/core/java/student/arfaz/forpattern/SeventyOnePro.java
@@ -1,6 +1,6 @@
 	package org.la.core.java.student.arfaz.forpattern;
 
-//Wap to check whether a number is Armstrong number or not.
+//Wap to check whether a number is Armstrong number or not 1 to n
 
 public class SeventyOnePro {
 	public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class SeventyOnePro {
 		   		arm =arm + rem*rem*rem;
 		   		m=m/10;
 			}
-				if(arm==m)
+				if(arm==i) // i here because i is original number to get after perform armstrong and m is not a original 
 			System.out.println(i);	
 		}
 		
