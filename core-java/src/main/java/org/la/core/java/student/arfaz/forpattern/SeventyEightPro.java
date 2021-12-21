d5a555bdd7fc5dbbbe0868530638bbbda3fac3be
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2021-12-21 10:26:29 +0530

    assign_4seventy_3 to Seventy eight

---
+++ core-java/src/main/java/org/la/core/java/student/arfaz/forpattern/SeventyEightPro.java
@@ -0,0 +1,26 @@
+	package org.la.core.java.student.arfaz.forpattern;
+
+/*  ****
+	***
+	**
+	*
+*/
+public class SeventyEightPro {
+	public static void main(String[] args) {
+		int n=5;
+			for(int i=n;i>=1;i--)  {
+				for(int j=1;j<=i;j++)  {
+				System.out.print("*");
+				}
+					System.out.println();
+				}
+					
+	
+				
+		
+	}
+
+		
+	
+		
+}
---
+++ core-java/src/main/java/org/la/core/java/student/arfaz/forpattern/SeventyNinePro.java
@@ -0,0 +1,34 @@
+	package org.la.core.java.student.arfaz.forpattern;
+
+/* 
+ * 
+ *  79.
+		****
+		 ***
+		  **
+		   *
+*/
+public class SeventyNinePro {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=1;i<=n;i++)  {
+			for(int j=1;j<i;j++)  {
+			System.out.print(" ");
+			}
+		}
+		for(int i=n;i>=1;i--)  {
+			for(int j=1;j<=i;j++)  {
+			System.out.print("*");
+			}
+				System.out.println();
+			}
+				
+		
+				
+		
+	}
+
+		
+	
+		
+}
