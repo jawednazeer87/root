7ce133e747002fccea0942e31072ae0bb809592e
mudassir <mudassirakhter420@gmail.com>
2021-12-24 10:39:06 +0530

    print the given progam

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap15.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 34;
+        int j = 7;
+        int k = i % j;
+        System.out.println("k = " + k );
+
+	}
+
+}
