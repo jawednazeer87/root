6e55378021eb36eb4db5e2cd19f513bfe3a19b7a
mudassir <mudassirakhter420@gmail.com>
2021-12-24 12:20:16 +0530

    wap to add two char variable and store sum in char variable and print

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap30.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char a = 'A', b = 'B';
+		char c=(char) (a + b);
+		System.out.println(c);
+	}
+
+}
