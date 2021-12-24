4aae50f02f5a9983f86912043d9affeae2296d52
mudassir <mudassirakhter420@gmail.com>
2021-12-24 12:17:11 +0530

    print program 27

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap27.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0152;
+		 int b = --a / 2 * 2;
+		 System.out.println("a=" + a);
+
+	}
+
+}
