6720269560cb6a0f59485a62326e0a3e5d604e4b
mudassir <mudassirakhter420@gmail.com>
2021-12-24 12:18:27 +0530

    wap to add two int variable but assign the result to double variable

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap28.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap28 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 547, y = 5456;
+		double result = x + y;
+		System.out.println(result);
+
+	}
+
+}
