8184996fb93f2d9e22a34447d783b01133711ec4
mudassir <mudassirakhter420@gmail.com>
2021-12-24 10:33:28 +0530

    what will be output: int x = 5 + 3 * 4 / 5 + 4;

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap12.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 5 + 3 * 4 / 5 + 4;
+		System.out.println(" Output : " + x);
+
+
+	}
+
+}
