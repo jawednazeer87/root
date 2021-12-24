335d3eac868c4f3b980bc7d2a15fd459761071c6
mudassir <mudassirakhter420@gmail.com>
2021-12-24 10:35:57 +0530

    what will be output: double x = 14 / 5 ;

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/Arthmetic/Wap13.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage.Arthmetic;
+
+public class Wap13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double x = 14 / 5 ;
+		System.out.println(" Output : " + x);
+		
+		
+
+	}
+
+}
