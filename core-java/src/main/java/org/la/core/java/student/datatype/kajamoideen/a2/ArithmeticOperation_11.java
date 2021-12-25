6e095b23217c714f895c9d8d31f7c59ae8832cdc
moideen <moideen@Admin>
2021-12-25 17:16:29 +0530

    printed avg of two numbers

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_11.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_11 {
+
+	public static void main(String[] args) {
+//11.	wap to find average of 2 numbers?
+		int a = 99, b = 9;
+		int average = (a + b) / 2;
+		System.out.println(average);
+	}
+
+}
