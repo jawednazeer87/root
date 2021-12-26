9f7d214e60a199eb36a7f360635d004303d4d3fc
moshin <moshin.ma715@gmail.com>
2021-12-26 21:10:23 +0530

    Assignment

---
+++ core-java/src/main/java/org/la/core/java/student/moshin/assignment4/Question047.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.moshin.assignment4;
+
+public class Question047 
+{
+	//47.	Wap to print all natural numbers in reverse (from n to 1).
+    public static void main(String[] args) {
+        for (int i = 10; i> 0 ; i--) {
+            System.out.println(i);
+        }
+
+	
+	}
+}
+
---
+++ core-java/src/main/java/org/la/core/java/student/moshin/assignment4/Question049.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.moshin.assignment4;
+
+public class Question049 
+{
+	//49.	Wap to print all even numbers between 1 to 100.
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+	}
+}
+
