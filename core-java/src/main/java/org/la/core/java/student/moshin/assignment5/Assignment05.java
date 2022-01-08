e6644ef881cc8d1fa17e3b1004d0b2e4cfef7163
moshin <moshin.ma715@gmail.com>
2022-01-08 18:18:12 +0530

    Assignment5

---
+++ core-java/src/main/java/org/la/core/java/student/moshin/assignment5/Assignment05.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.moshin.assignment5;
+
+public class Assignment05 {
+	
+	public static void main(String[] args) {
+		
+		int[] arr = new int[10];
+		int j=1;
+		for(int i=0; i<arr.length;i++) {
+			arr[i] = j;
+			j += 1;
+			if(arr[i]%2 == 0)
+			System.out.println("arr["+i+"]= "+arr[i]);
+		}
+
+	}
+
+}
