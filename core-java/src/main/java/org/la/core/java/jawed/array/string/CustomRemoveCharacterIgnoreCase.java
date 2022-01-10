1442e354c732155d802e6fa0c8a451146ea79828
jawednazeer87 <jawednazeer87@gmail.com>
2022-01-10 09:08:33 +0800

    fix bugs on removing specific character ignore case

--- core-java/src/main/java/org/la/core/java/jawed/array/string/CustomRemoveCharacterIgnoreCase.java
+++ core-java/src/main/java/org/la/core/java/jawed/array/string/CustomRemoveCharacterIgnoreCase.java
@@ -8,18 +8,35 @@ public class CustomRemoveCharacterIgnoreCase {
 	
 	public static void main(String[] args) {
 		String src = "3Hello# how2 are yo?u 4334";
-		char ch = 'h';
+		char ch = '3';
 		String reverseWords = removeSpecialCharacter(src, ch);
 		System.out.println(reverseWords);
 	}
 		
 	static String removeSpecialCharacter(String src, char ch) {
+		char ch2 = ' ';
+		boolean alphabet = false;
+		if((ch>='a' && ch<='z')) {
+			ch2 = (char)(ch-32);
+			alphabet = true;
+		}
+		else if(ch>='A' && ch<='Z') {
+			ch2 = (char)(ch+32);
+			alphabet = true;
+		}
 		char arr[] = src.toCharArray();
 		int length = src.length();
 		StringBuilder sb = new StringBuilder();
 		for(int i=0 ; i<length ; i++) {
-			if(!(arr[i]==ch || arr[i]==(ch-32))) {
-				sb.append(arr[i]);
+			if(alphabet) {
+				if(!(arr[i]==ch || arr[i]==ch2)) {
+					sb.append(arr[i]);
+				}
+			}
+			else {
+				if(arr[i]!=ch) {
+					sb.append(arr[i]);
+				}
 			}
 		}
 		return String.valueOf(sb);
