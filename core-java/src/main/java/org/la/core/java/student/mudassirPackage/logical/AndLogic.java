d542f89ba0226126b5b4a8bb92b9c00693f2487f
mudassir <mudassirakhter420@gmail.com>
2022-01-15 15:17:06 +0530

    logical AND

--- core-java/src/main/java/org/la/core/java/student/mudassirPackage/logical/AndLogic.java
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/logical/AndLogic.java
@@ -4,7 +4,7 @@ public class AndLogic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        // Logical AND
 		boolean res = true && false;
 		System.out.println("res: "+res);
 		res = true && true;
