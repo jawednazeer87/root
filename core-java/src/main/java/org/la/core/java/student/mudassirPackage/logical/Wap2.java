5eccd861a76805f68437beebe94c9e06e77a7d65
mudassir <mudassirakhter420@gmail.com>
2022-01-15 15:49:09 +0530

    logical operator 2

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/logical/Wap2.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.mudassirPackage.logical;
+
+public class Wap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x=1, y=2;
+		boolean b = x!=y || ++y==3;
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+		System.out.println("b: "+b);
+
+	}
+
+}
