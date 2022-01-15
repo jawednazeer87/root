368002cdc46e7a5f51f8ee8ee84084344ff37635
mudassir <mudassirakhter420@gmail.com>
2022-01-15 15:15:02 +0530

    Logical AND

---
+++ core-java/src/main/java/org/la/core/java/student/mudassirPackage/logical/AndLogic.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.mudassirPackage.logical;
+
+public class AndLogic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean res = true && false;
+		System.out.println("res: "+res);
+		res = true && true;
+		System.out.println("res: "+res);
+
+	}
+
+}
