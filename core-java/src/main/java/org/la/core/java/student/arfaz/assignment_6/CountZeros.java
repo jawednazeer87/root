c0797199a2ff3173824115ae0a4b17e0dcc7b7de
arfaz9030 <60770097+arfaz9030@users.noreply.github.com>
2022-01-15 12:40:25 +0530

    Assign-6-Remaining 6 programs

---
+++ core-java/src/main/java/org/la/core/java/student/arfaz/assignment_6/CountZeros.java
@@ -0,0 +1,30 @@
+package org.la.core.java.student.arfaz.assignment_6;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * 
+	
+	
+int countZero(int a[])  a[] = {1,0,1,0,0,1,1,0};
+		count number of zeros and return 
+*/
+
+public class CountZeros {
+//	static Logger log = LoggerFactory.getLogger(PrintStarReverseRight.class);
+
+	public static void main(String[] args) {
+		int a[]= {1,0,1,0,0,1,1,0};
+		countOnes(a);
+	}
+	static  void countOnes(int a[]) {
+		int c=0;
+		for(int i=0;i<a.length;i++)  {
+			if(a[i]==1)
+				c++;	
+		}
+		System.out.println(c);
+	}
+	
+}
