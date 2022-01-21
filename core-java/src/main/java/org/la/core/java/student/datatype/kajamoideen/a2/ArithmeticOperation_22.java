09e4de05d4d501ef9885bf9e9126a9ed3390ef12
moideen <moideen@Admin>
2022-01-21 21:55:58 +0530

    printed output

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_22.java
@@ -0,0 +1,17 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+        a++;
+        a += a;
+        --a;
+        a = 7 + a;
+        a *= a;
+        a -= 3;
+		System.out.println(" a = " + a );
+	}
+
+}
