c5d3cc68dc2e2a138a8dc055c2d9fe335743d958
shoaibisa <shoaibisa1@gmail.com>
2022-01-22 00:44:40 +0530

    Q40

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/bitWiseOperator/Q40.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.shoaibisaPackage.bitWiseOperator;
+
+public class Q40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 3;
+		
+			int res = n<<4; //right shift
+			System.out.print(res);
+		
+
+	}
+
+}
