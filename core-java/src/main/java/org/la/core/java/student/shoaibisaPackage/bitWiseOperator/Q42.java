191fcc0202fe143fb8bf21e21f56439abea944c6
shoaibisa <shoaibisa1@gmail.com>
2022-01-22 00:48:37 +0530

    Q46 bitwise

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/bitWiseOperator/Q42.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.shoaibisaPackage.bitWiseOperator;
+
+public class Q42 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 1;
+		
+		int res = n<<2; //right shift
+		System.out.print(res);
+	}
+}
---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/bitWiseOperator/Q46.java
@@ -0,0 +1,11 @@
+package org.la.core.java.student.shoaibisaPackage.bitWiseOperator;
+
+public class Q46 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 20;
+		
+		int res = n<<5; //right shift
+		System.out.print(res);
+	}
+}
