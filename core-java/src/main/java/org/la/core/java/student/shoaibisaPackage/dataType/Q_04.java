5cf9b4a2d660f28fe1add618fa4cf28e191a3cfe
shoaibisa <shoaibisa1@gmail.com>
2022-01-22 00:10:37 +0530

    updating

--- core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/dataType/Q_04.java
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/dataType/Q_04.java
@@ -1,11 +1,11 @@
-package org.la.core.java.student.shoaibisaPackage;
+package org.la.core.java.student.shoaibisaPackage.dataType;
 
-public class Q_03 {
+public class Q_04 {
 
 	public static void main(String[] args) {
-	
+
 		System.out.println("java class");
-		
+
 	}
 
 }
--- core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/dataType/Q_09.java
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/dataType/Q_09.java
@@ -1,4 +1,4 @@
-package org.la.core.java.student.shoaibisaPackage;
+package org.la.core.java.student.shoaibisaPackage.dataType;
 
 public class Q_09 {
 
