19df40112388c82e5900afa0bb73c6ddd997258a
moideen <moideen@Admin>
2022-01-28 16:22:40 +0530

    printed output

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_26.java
@@ -0,0 +1,13 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_26 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s = 10;
+        int a = s;
+        s = (short) (s + 10);
+        System.out.println(s + "," + a);
+	}
+
+}
