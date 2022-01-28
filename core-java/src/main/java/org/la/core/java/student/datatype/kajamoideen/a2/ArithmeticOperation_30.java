b56ccd47b04def18c737654a24c6d541927a3207
moideen <moideen@Admin>
2022-01-28 16:38:39 +0530

    printed ouput

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_30.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_30 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//30.	wap to add two char variable and store 
+		//sum in char variable and print the result?
+		char a='a',b='9';
+		char c=(char) (a+b);
+		System.out.println(c);
+		
+	}
+
+}
