095d5aa1ee785407d144132687143f4bd5325114
sajferoz <92019439+sajferoz@users.noreply.github.com>
2022-01-29 21:05:46 +0800

    Sajid-Assignment6-Completed

---
+++ core-java/src/main/java/org/la/core/java/student/sajid/method/assignment6/Equation42.java
@@ -0,0 +1,22 @@
+package org.la.core.java.student.sajid.method.assignment6;
+
+public class Equation42 {
+	
+    public static void main(String s[])
+    {
+        int x = 6;
+        int y = 3;
+        int z = 12;
+
+        int w = method1(x, y, z);
+        
+        System.out.println("w = " + w);
+    }
+
+    public static int method1(int y, int z, int x)
+    {
+        return (x * z) / y;
+      
+    }
+
+}
