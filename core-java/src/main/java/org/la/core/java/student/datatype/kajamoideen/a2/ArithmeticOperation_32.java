e802877bbdcab0dc8c62e287beeb7ef0c909437e
moideen <moideen@Admin>
2022-01-30 21:07:39 +0530

    printed output

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_32.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_32 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//32.	wap to add two double variable but assign
+		//the result to int variable and print the result?
+		double a=5.11f;
+		double b=41.22f;
+		int integerSum = (int) (a+b);
+		System.out.println(integerSum);
+		
+	}
+
+}
