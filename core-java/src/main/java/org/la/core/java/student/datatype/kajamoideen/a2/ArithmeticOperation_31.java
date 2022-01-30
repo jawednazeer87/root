fbe826e7d38bbad08d068c224a3161bbe3a83631
moideen <moideen@Admin>
2022-01-30 21:02:18 +0530

    printed output

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/ArithmeticOperation_31.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class ArithmeticOperation_31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//31.	wap to add two float variable but assign the result 
+		//to int variable and print the result?
+		float a=1.1f;
+		float b=2.2f;
+		int integerSum = (int) (a+b);
+		System.out.println(integerSum);
+		
+	}
+
+}
