e5d331765a15554c363a600f65eb399225685cda
moideen <moideen@Admin>
2022-01-30 21:17:08 +0530

    printed output

---
+++ core-java/src/main/java/org/la/core/java/student/datatype/kajamoideen/a2/IncDecOperator_1.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.datatype.kajamoideen.a2;
+
+public class IncDecOperator_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//1.	wap to do pre increment a number and assign its value to other 
+		//number and print both number
+		int a =10, b=++a;
+		System.out.println("a = "+ a+" b = "+ b);
+		
+	}
+
+}
