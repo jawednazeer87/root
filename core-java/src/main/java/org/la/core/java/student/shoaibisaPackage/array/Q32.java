6e3ede5888b064aec61a82bd4e08b58a2d61097f
jawednazeer87 <jawednazeer87@gmail.com>
2022-02-12 21:59:18 +0800

    fixing bug

--- core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/array/Q32.java
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/array/Q32.java
@@ -5,7 +5,9 @@ public class Q32 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	//	int res = 90 & 08;  //OUT OF RANGE CZ ITS NOT BINARY NUMBER
-		for(int i=0; i<)
+		for(int i=0; i<10 ; i++) {
+			
+		}
 
 	}
 
