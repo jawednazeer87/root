674838f97896d37e23c454960277743c64412e18
AbrahamWang <wang.ibrahim222@gmail.com>
2022-02-12 00:26:28 -0800

    Assignment 7 updated

--- core-java/src/main/java/org/la/core/java/student/datatype/wang/assignment7/Assignment57.java
+++ core-java/src/main/java/org/la/core/java/student/datatype/wang/assignment7/Assignment57.java
@@ -6,6 +6,8 @@ public class Assignment57 {
     public static void main(String[] args) {
         int res = ~20; //~ 00010100 =11101011
         System.out.println(res); // -21
+        int res1 = ~-12; //~ 00010100 =11101011
+        System.out.println(res1);
 
     }
 }
