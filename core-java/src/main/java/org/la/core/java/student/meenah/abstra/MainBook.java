2ee32b57bcbf742005949484a0809e6c9b919067
meena <eng.meena28@gmail.com>
2022-02-24 22:43:13 +0800

    Override abstract methods

---
+++ core-java/src/main/java/org/la/core/java/student/meenah/abstra/MainBook.java
@@ -0,0 +1,16 @@
+package org.la.core.java.student.meenah.abstra;
+
+public class MainBook {
+
+	public static void main(String[] args) {
+		
+		
+		Book b1 = new MathBook();
+		b1.read();
+		
+		Book b2 = new EnglishBook();
+		b2.read();
+
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/meenah/abstra/MathBook.java
@@ -0,0 +1,14 @@
+package org.la.core.java.student.meenah.abstra;
+
+public class MathBook extends Book {
+
+	
+	public MathBook() {
+	}
+		
+		@Override
+		public void read() {
+			System.out.println("This is abstarct mothod overrided from  abstact Book class");
+		}
+	
+}
