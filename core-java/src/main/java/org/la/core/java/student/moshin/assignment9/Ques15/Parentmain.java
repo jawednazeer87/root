c74efcc9f7825417684e25025f3f196b238898a9
moshin <moshin.ma715@gmail.com>
2022-03-04 17:06:13 +0530

    Assignment 9

--- core-java/src/main/java/org/la/core/java/student/moshin/assignment9/Ques15/Parentmain.java
+++ core-java/src/main/java/org/la/core/java/student/moshin/assignment9/Ques15/Parentmain.java
@@ -2,7 +2,7 @@ package org.la.core.java.student.moshin.assignment9.Ques15;
 
 public class Parentmain {
 	public static void main(String[] args) {
-		Parent pa = new Parent();
+		Parent pa = new child();
 		int a[] = new int[10];
 		for(int i=0;i<9;i++)
 		a[i] = i;
---
+++ core-java/src/main/java/org/la/core/java/student/moshin/assignment9/Ques15/child.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.moshin.assignment9.Ques15;
+
+public class child extends Parent {
+	   int[] copy(int[]a){
+			int b[] = new int[10];
+			for(int i=0;i<a.length;i++)
+				b[i]=a[i];
+				
+			   return b;	
+				  	
+			}
+}
