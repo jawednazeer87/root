46cd322f0e3ee6bdc29b9f9465f5f8ecc46a78fc
jawednazeer87 <jawednazeer87@gmail.com>
2022-03-05 16:55:59 +0800

    dot object to object mapping

--- core-java/src/main/java/org/la/core/java/jawed/lambda/StudentMain.java
+++ core-java/src/main/java/org/la/core/java/jawed/lambda/StudentMain.java
@@ -7,7 +7,11 @@ import java.util.List;
 
 import org.la.core.java.jawed.common.Constants;
 import org.la.core.java.jawed.model.Student;
+import org.la.core.java.jawed.model.dto.StudentDTO;
 import org.la.core.java.jawed.util.GSONGenericUtil;
+import org.la.core.java.jawed.util.StudentUtil;
+
+import com.google.gson.Gson;
 
 /**
  * @author jawednazeer
@@ -15,11 +19,36 @@ import org.la.core.java.jawed.util.GSONGenericUtil;
  */
 public class StudentMain {
 	public static void main(String[] args) {
-		GSONGenericUtil<Student> gu = new GSONGenericUtil<>(Student.class);
-		Student student = gu.jsonToObject(Constants.STUDENT_FILE_NAME);
-		//System.out.println("main: "+student);
+		//conversionManual();
+		conversionByGSON();
+	}
+	
+	//reading json from files and converting to java object
+	public static Student jsonToObject() {
+		Student student = GSONGenericUtil.jsonToObject(Constants.STUDENT_FILE_NAME, Student.class);
+		System.out.println("main: "+student);
+		return student;
+	}
+	
+	//reading json from files and converting to java list object
+	public static void jsonToListObject() {
 		List<Student> list = GSONGenericUtil.getList(Constants.STUDENT_LIST_FILE_NAME);
 		System.out.println("list: "+list);
-		//GenericUtil.print(list);
+	}
+	
+	//manual data conversion from User to UserDTO
+	public static void conversionManual() {
+		StudentDTO studentDTO = GSONGenericUtil.jsonToObject(Constants.STUDENT_FILE_NAME, StudentDTO.class);
+		Student student = StudentUtil.userDTOToUser(studentDTO);
+		System.out.println("manual: "+student);
+	}
+	
+	//using gson data conversion from User to UserDTO
+	public static void conversionByGSON() {
+		Gson gson = new Gson();
+		StudentDTO studentDTO = GSONGenericUtil.jsonToObject(Constants.STUDENT_FILE_NAME, StudentDTO.class);
+		String json = gson.toJson(studentDTO);
+		Student student = gson.fromJson(json, Student.class); 
+		System.out.println("by gson: "+student);
 	}
 }
--- core-java/src/main/java/org/la/core/java/jawed/util/StudentUtil.java
+++ core-java/src/main/java/org/la/core/java/jawed/util/StudentUtil.java
@@ -1,9 +1,25 @@
 package org.la.core.java.jawed.util;
 
+import org.la.core.java.jawed.common.Constants;
+import org.la.core.java.jawed.model.Student;
+import org.la.core.java.jawed.model.dto.StudentDTO;
+
 /**
  * @author jawednazeer
  */
 public class StudentUtil {
 
-	
+	//manual copying member variables value
+	public static Student userDTOToUser(StudentDTO studentDTO) {
+		Student student = new Student();
+		student.setId(studentDTO.getId());
+		student.setAge(studentDTO.getAge());
+		student.setRoll(studentDTO.getRoll());
+		student.setName(studentDTO.getName());
+		student.setEmail(studentDTO.getEmail());
+		student.setSchool(studentDTO.getSchool());
+		student.setStandard(studentDTO.getStandard());
+		student.setWeight(studentDTO.getWeight());
+		return student;
+	}
 }
