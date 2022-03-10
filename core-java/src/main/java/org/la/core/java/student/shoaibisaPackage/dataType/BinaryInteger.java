3bde0c1e4e79960091a627e553efc1fbc1c4c410
shoaibisa <shoaibisa1@gmail.com>
2022-03-10 22:31:20 +0530

    int to bin

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/dataType/BinaryInteger.java
@@ -0,0 +1,12 @@
+package org.la.core.java.student.shoaibisaPackage.dataType;
+
+public class BinaryInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		String binA = Integer.toBinaryString(a);
+		System.out.print(binA);
+	}
+
+}
