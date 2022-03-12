7530724ac75b72c9dad5895064c548e0c15985d0
shoaibisa <shoaibisa1@gmail.com>
2022-03-12 16:25:16 +0530

    practice

---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/classPolymorphism/FormattedOutuput.java
@@ -0,0 +1,15 @@
+package org.la.core.java.student.shoaibisaPackage.classPolymorphism;
+
+public class FormattedOutuput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		float b = 3298.4f;
+		
+		System.out.printf("%1$f %1$e",b);
+
+	}
+
+}
---
+++ core-java/src/main/java/org/la/core/java/student/shoaibisaPackage/incrementDecrement/FlagAndWidthFS.java
@@ -0,0 +1,18 @@
+package org.la.core.java.student.shoaibisaPackage.incrementDecrement;
+
+public class FlagAndWidthFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 19446;
+		int b=3;
+		
+		
+		//%5d has 5 spaces
+		
+		System.out.printf("%2$d%1$5d",a,b);
+
+	}
+
+}
