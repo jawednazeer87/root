23791a047ff8340b51d3ac54509137dfde3ab958
moshin <moshin.ma715@gmail.com>
2022-05-14 13:23:20 +0530

    Assignment

---
+++ advance-java/src/org/jn/adv/student/moshin/jdbc/controller/EmployeeController.java
@@ -0,0 +1,54 @@
+package org.jn.adv.student.moshin.jdbc.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jn.adv.student.moshin.jdbc.dto.EmployeeDTO;
+import org.jn.adv.student.moshin.jdbc.model.Employee;
+import org.jn.adv.student.moshin.jdbc.service.EmployeeService;
+import org.jn.adv.student.moshin.jdbc.util.GSONGenericUtil;
+
+public class EmployeeController {
+		static EmployeeService employeeService = new EmployeeService();
+		public static void main(String[] args) {
+			create();
+//			EmployeeDTO dto = GSONGenericUtil.map(getAll().get(5), EmployeeDTO.class);
+//			System.out.println(dto);
+//			getAll().stream().forEach(System.out::println);
+//			getByDobRange().stream().forEach(System.out::println);
+		}
+		
+		static void create() {
+			Employee employee = new Employee();
+			employee.setCompanyId(1);
+			employee.setFirstName("moshin");
+			employee.setLastName("ansari");
+			LocalDate localDate = LocalDate.of(2000, 07, 21);
+			employee.setDob(localDate);
+			employee.setSalary(90000.0);
+			employee.setGender("male"); 			//true-> male, false-> female
+			employeeService.create(employee);
+			employeeService.connectionClose();
+		}
+		
+		static List<EmployeeDTO> getAll() {
+			List<Employee> employeeList = employeeService.getAll();
+			employeeService.connectionClose();
+			final List<EmployeeDTO> dtoList = new ArrayList<>();
+			employeeList.stream().forEach(employee-> {
+				EmployeeDTO dto = GSONGenericUtil.map(employee, EmployeeDTO.class);
+				dtoList.add(dto);
+			});
+			return dtoList;
+		}
+		
+		static List<EmployeeDTO> getByDobRange() {
+			List<Employee> employeeList = employeeService.getByDobRange(LocalDate.of(1979, 01, 10), 
+																		LocalDate.of(2021, 01, 10));
+			employeeService.connectionClose();
+			List<EmployeeDTO> dtoList = GSONGenericUtil.mapList(employeeList, EmployeeDTO.class);
+			return dtoList;
+		}
+
+}
---
+++ advance-java/src/org/jn/adv/student/moshin/jdbc/util/GenericUtil.java
@@ -0,0 +1,18 @@
+package org.jn.adv.student.moshin.jdbc.util;
+
+import java.util.List;
+
+/**
+ * @author jawednazeer
+ */
+public class GenericUtil {
+	public static <T> void print(List<T> list) {
+		System.out.println("[");
+		for(T t : list) {
+			System.out.println(t);
+		}
+		System.out.println("]");
+	}
+	
+	
+}
