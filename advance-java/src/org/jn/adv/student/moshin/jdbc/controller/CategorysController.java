7af14c1626f8e4232f52ab8395af01767c82514c
moshin <moshin.ma715@gmail.com>
2022-05-21 18:47:19 +0530

    Assignment JDBC

---
+++ advance-java/src/org/jn/adv/student/moshin/jdbc/controller/CategorysController.java
@@ -0,0 +1,56 @@
+package org.jn.adv.student.moshin.jdbc.controller;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jn.adv.student.moshin.jdbc.dto.BrandsDTO;
+import org.jn.adv.student.moshin.jdbc.dto.CountriesDTO;
+import org.jn.adv.student.moshin.jdbc.dto.EmployeeDTO;
+import org.jn.adv.student.moshin.jdbc.model.Brands;
+import org.jn.adv.student.moshin.jdbc.model.Countries;
+import org.jn.adv.student.moshin.jdbc.model.Employee;
+import org.jn.adv.student.moshin.jdbc.service.BrandsService;
+import org.jn.adv.student.moshin.jdbc.service.CountriesService;
+import org.jn.adv.student.moshin.jdbc.service.EmployeeService;
+import org.jn.adv.student.moshin.jdbc.util.GSONGenericUtil;
+
+public class CategorysController {
+		static BrandsService brandsService = new BrandsService();
+		public static void main(String[] args) {
+			create();
+//			BrandsDTO dto = GSONGenericUtil.map(getAll().get(5), BrandsDTO.class);
+//			System.out.println(dto);
+//			getAll().stream().forEach(System.out::println);
+//			getByDobRange().stream().forEach(System.out::println);
+		}
+		
+		static void create() {
+			Brands brands = new Brands();
+			brands.setId(12221);
+			brands.setBrand_name("india");
+			brands.setThumbnails("aisa");
+			brandsService.create(brands);
+			brandsService.connectionClose();
+		}
+		
+		static List<BrandsDTO> getAll() {
+			List<Brands> brandsList = brandsService.getAll();
+			brandsService.connectionClose();
+			final List<BrandsDTO> dtoList = new ArrayList<>();
+			brandsList.stream().forEach(brands-> {
+				BrandsDTO dto = GSONGenericUtil.map(brands, BrandsDTO.class);
+				dtoList.add(dto);
+			});
+			return dtoList;
+		}
+		
+		static List<BrandsDTO> getByDobRange() {
+			List<Brands> brandsList = brandsService.getByDobRange(LocalDate.of(1979, 01, 10), 
+																		LocalDate.of(2021, 01, 10));
+			brandsService.connectionClose();
+			List<BrandsDTO> dtoList = GSONGenericUtil.mapList(brandsList, BrandsDTO.class);
+			return dtoList;
+		}
+
+}
---
+++ advance-java/src/org/jn/adv/student/moshin/jdbc/model/Categorys.java
@@ -0,0 +1,60 @@
+package org.jn.adv.student.moshin.jdbc.model;
+
+import java.time.LocalDate;
+public class Categorys {
+
+	private Integer id;
+	private String brand_name;
+	private String thumbnails;
+
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+
+	/**
+	 * @return the brand_name
+	 */
+	public String getBrand_name() {
+		return brand_name;
+	}
+
+	/**
+	 * @param brand_name the brand_name to set
+	 */
+	public void setBrand_name(String brand_name) {
+		this.brand_name = brand_name;
+	}
+
+	/**
+	 * @return the thumbnails
+	 */
+	public String getThumbnails() {
+		return thumbnails;
+	}
+
+	/**
+	 * @param thumbnails the thumbnails to set
+	 */
+	public void setThumbnails(String thumbnails) {
+		this.thumbnails = thumbnails;
+	}
+
+	@Override
+	public String toString() {
+		return "{id: "+id+", brand_name: "+brand_name+", thumbnails: "+thumbnails+"}";
+	}
+
+}
