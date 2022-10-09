e28cac6a1ba6126680d063dc121f621a0db24346
azhan <azhan@LAPTOP-G5S927UO>
2022-10-10 00:17:14 +0530

    Variables and arithmetic.

---
+++ core-java/src/main/java/org/jn/core/java/student/azhan/datatype/Variable.java
@@ -0,0 +1,18 @@
+package org.jn.core.java.student.azhan.datatype;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		char c = 'm';
+		double d = 50.22;
+		float s = 30.45f;
+		boolean b = true;
+		System.out.println("a = "+a);
+		System.out.println("c = "+c);
+		System.out.println("d = "+d);
+		System.out.println("s = "+s);
+		System.out.println("b = "+b);
+	}
+}
