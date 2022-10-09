09ba3b9e88099cc3bb83a40d782f8a42a541fccd
jawednazeer87 <jawednazeer87@gmail.com>
2022-10-09 17:04:40 +0800

    first program of new class

---
+++ core-java/src/main/java/org/jn/core/java/jawed/datatype/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.jn.core.java.jawed.datatype;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("First java class");
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/datatype/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.jn.core.java.student.datatype;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+		System.out.println("First java class");
+	}
+
+}
