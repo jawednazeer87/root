aa6dbc9136810dafa12d11114cae1a07d56a5528
Sadullah <Sadullah@localhost>
2022-10-10 19:41:17 +0530

    Trying out some of the arithmetic operators

---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/datatype/ArithmeticOperations.java
@@ -0,0 +1,16 @@
+package org.jn.core.java.student.sadullah.datatype;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		int a = 50;
+		int b = 70;
+		int sum = a+b;
+		int subtraction = a-b;
+		int multiplication = a*b;
+		System.out.println("The sum of a & b is "+sum);
+		System.out.println("The difference between a & b is "+subtraction);
+		System.out.println("The product of a & b is "+multiplication);		
+	}
+
+}
