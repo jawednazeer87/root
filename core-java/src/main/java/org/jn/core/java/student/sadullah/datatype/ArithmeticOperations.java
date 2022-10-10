83c77ef3aebb8cb8504454aeb30cd8bb46c07be1
Sadullah <Sadullah@localhost>
2022-10-10 19:51:35 +0530

    Trying out some of the arithmetic operators

--- core-java/src/main/java/org/jn/core/java/student/sadullah/datatype/ArithmeticOperations.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/datatype/ArithmeticOperations.java
@@ -5,12 +5,9 @@ public class ArithmeticOperations {
 	public static void main(String[] args) {
 		int a = 50;
 		int b = 70;
-		int sum = a+b;
-		int subtraction = a-b;
-		int multiplication = a*b;
-		System.out.println("The sum of a & b is "+sum);
-		System.out.println("The difference between a & b is "+subtraction);
-		System.out.println("The product of a & b is "+multiplication);		
+		System.out.println("The sum of a & b is "+ (a+b));
+		System.out.println("The difference between a & b is "+(a-b));
+		System.out.println("The product of a & b is "+(a*b));		
 	}
 
 }
