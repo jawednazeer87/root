d7eb77de77c69d6ccc80f836fdc3375eaa4d1cd9
Arsallan Shahab <mdarsallan@gmail.com>
2022-10-10 21:09:44 +0530

    created first program in java and printed hello world.

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/FirstProgram.java
@@ -0,0 +1,10 @@
+package org.jn.core.java.student.arsallan.shahab.datatype;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World :)");
+	}
+	
+
+}
