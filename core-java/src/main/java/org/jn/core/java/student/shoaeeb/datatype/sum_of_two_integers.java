f8ec54eb6a13ab3de4c6e4ddc53c69b8d1713c61
shoaeeb <osman.shoaeeb@gmail.com>
2022-10-12 16:54:28 +0530

    Assignment-1(Data types in java)

---
+++ core-java/src/main/java/org/jn/core/java/student/shoaeeb/datatype/sum_of_two_integers.java
@@ -0,0 +1,13 @@
+package org.jn.core.java.student.shoaeeb.datatype;
+
+public class sum_of_two_integers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		int sum = a + b;
+		System.out.println("Sum is "+sum);
+	}
+
+}
