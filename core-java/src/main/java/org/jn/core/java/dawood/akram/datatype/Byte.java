8d0664db894fd166a86d6f039a7c39979febbfdf
Personal <Personal@DESKTOP-L12EV7F>
2022-10-14 20:22:20 +0530

    Byte

---
+++ core-java/src/main/java/org/jn/core/java/dawood/akram/datatype/Byte.java
@@ -0,0 +1,11 @@
+package org.jn.core.java.dawood.akram.datatype;
+
+public class Byte {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           byte b = 20;
+           System.out.println();
+	}
+
+}
