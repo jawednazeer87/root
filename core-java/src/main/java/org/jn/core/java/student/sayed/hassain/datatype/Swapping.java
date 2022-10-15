9055c6dd52b2de8c22ee7a426a07b70cc22bfd31
geeksyed <sayedhassain14@gmail.com>
2022-10-15 22:58:22 +0530

    swapping program added

--- core-java/src/main/java/org/jn/core/java/student/sayed/hassain/datatype/Swapping.java
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/datatype/Swapping.java
@@ -4,7 +4,14 @@ public class Swapping {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num1=56, num2=73, a;
+		
+		System.out.println("Original numbers are "+ num1+ " " +num2);
+		//swapping logic
+		a=num1;
+		num1=num2;
+		num2=a;
+		System.out.println("Swapped numbers are "+ num1+ " " +num2);
 	}
 
 }
