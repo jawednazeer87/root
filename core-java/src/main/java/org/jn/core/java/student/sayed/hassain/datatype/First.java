c260506b89b87c4a125bfe7ff894c09e5daf4505
geeksyed <sayedhassain14@gmail.com>
2022-10-15 13:23:37 +0530

    This is my First Program

--- core-java/src/main/java/org/jn/core/java/student/sayed/hassain/datatype/First.java
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/datatype/First.java
@@ -2,6 +2,6 @@ package org.jn.core.java.student.sayed.hassain.datatype;
 
 public class First {
 	public static void main(String[] args) {
-		System.out.println("Hello, World! Hi I am your friend.");
+		System.out.println("Hello, World! Hi I am your friend. How are you?");
 	}
 }
