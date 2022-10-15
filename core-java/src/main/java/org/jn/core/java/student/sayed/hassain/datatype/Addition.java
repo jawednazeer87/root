a7579d9a92eeaab4b64fe7728d148fd1bf462b71
geeksyed <sayedhassain14@gmail.com>
2022-10-15 13:13:48 +0530

    Addition Program

---
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/datatype/Addition.java
@@ -0,0 +1,20 @@
+package org.jn.core.java.student.sayed.hassain.datatype;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// declare variables
+		int num1, num2, sum;
+
+		// take two numbers
+		num1 = 10;
+		num2 = 20;
+		
+		//calculate sum value
+		sum=num1+num2;
+		
+		//display the sum value
+		System.out.println("Sum="+sum);
+
+	}
+}
