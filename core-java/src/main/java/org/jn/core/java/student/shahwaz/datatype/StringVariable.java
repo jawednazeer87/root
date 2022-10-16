f8aafe5e1cddf7e7a2c4ae10bb146df8899083a4
Shahwaz <mdsaif52565@gmail.com>
2022-10-16 16:56:18 +0530

    string variable

---
+++ core-java/src/main/java/org/jn/core/java/student/shahwaz/datatype/StringVariable.java
@@ -0,0 +1,12 @@
+package org.jn.core.java.student.shahwaz.datatype;
+
+public class StringVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="Shahwaz";
+		System.out.println("my name is " +name);
+		
+	}
+
+}
