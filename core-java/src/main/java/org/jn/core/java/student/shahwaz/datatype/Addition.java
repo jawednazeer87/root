f8ca206855de521f8613dc80cca78740913fa3cb
Shahwaz <mdsaif52565@gmail.com>
2022-10-16 17:05:46 +0530

    Addition of two

---
+++ core-java/src/main/java/org/jn/core/java/student/shahwaz/datatype/Addition.java
@@ -0,0 +1,16 @@
+package org.jn.core.java.student.shahwaz.datatype;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=3;
+		int b=2;
+		int sum=a+b;
+		//System.out.println("the addtion of a+b= "+sum);
+		System.out.printf("the sum of %d and %d is %d",a,b,sum);
+		
+
+	}
+
+}
