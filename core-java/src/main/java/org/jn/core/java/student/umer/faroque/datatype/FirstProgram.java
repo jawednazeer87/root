cd113614b3876b322a8f1506d4301ed973440f99
umer <umerfaroque20@gmail.com>
2022-10-16 07:03:51 +0530

    first program added

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/datatype/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.jn.core.java.student.umer.faroque.datatype;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("First java class");
+	}
+
+}
