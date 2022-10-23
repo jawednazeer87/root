a0a04c64dfd3a1910e099cbb481507971aebdbaa
Mohammed Afan <Mohammed Afan@LAPTOP-EH76GOUE>
2022-10-23 15:50:20 +0530

    First program

---
+++ core-java/src/main/java/student/afan/datatype/Arithmetic_operations.java
@@ -0,0 +1,13 @@
+package student.afan.datatype;
+public class Arithmetic_operations {
+public static void main(String[] args) {
+	int a=20;int b=20;int sum=0;int sub=0;int div=0;
+	sum=a+b;
+	sub=a-b;
+	div=a/b;
+	System.out.println("sum ="+(sum));
+	System.out.println("sub="+(sub));
+	System.out.println("div="+(div));
+	}
+
+}
---
+++ core-java/src/main/java/student/afan/datatype/Helloworld.java
@@ -0,0 +1,9 @@
+package student.afan.datatype;
+public class Helloworld
+{
+	public static void main(String[] args) {
+		System.out.println("afan");
+
+	}
+
+}
