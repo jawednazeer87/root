df785e772b0a63e6d9a66d6a3fe23af07d96ccd9
jawednazeer87 <jawednazeer87@gmail.com>
2022-10-23 23:30:24 +0800

    programs on unary operators, binary conversion and type casting

---
+++ core-java/src/main/java/org/jn/core/java/jawed/operators/unary/DecrementExample.java
@@ -0,0 +1,13 @@
+package org.jn.core.java.jawed.operators.unary;
+
+public class DecrementExample {
+
+	public static void main(String[] args) {
+		int x = 3;
+		x++;
+		System.out.println(x);
+		int y = -20;
+		y++;
+		System.out.println(y);
+	}
+}
---
+++ core-java/src/main/java/org/jn/core/java/jawed/type/cast/Division.java
@@ -0,0 +1,12 @@
+package org.jn.core.java.jawed.type.cast;
+
+public class Division {
+
+	public static void main(String[] args) {
+		double div = 10/100;
+		System.out.println(div);
+		
+		div = 10/100.0;
+		System.out.println(div);
+	}
+}
