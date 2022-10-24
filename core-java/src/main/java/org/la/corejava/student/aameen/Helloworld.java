e84ce38c93355a98cd0aba34df7c6dc12c0c5bab
AAMEEN <AAMEEN@LAPTOP-TPIV9VVE.domain.name>
2022-10-25 00:13:31 +0530

    wap to print: hello world

---
+++ core-java/src/main/java/org/la/corejava/student/aameen/Helloworld.java
@@ -0,0 +1,13 @@
+package org.la.corejava.student.aameen;
+
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// wap to print: hello world
+		
+		System.out.println("Hello world ");
+
+	}
+
+}
