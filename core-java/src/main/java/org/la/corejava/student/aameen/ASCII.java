964282647f6747e9cf55622fff03ceae17adda89
AAMEEN <AAMEEN@LAPTOP-TPIV9VVE.domain.name>
2022-10-25 13:58:35 +0530

    wap to print number as char and its ascii value

---
+++ core-java/src/main/java/org/la/corejava/student/aameen/ASCII.java
@@ -0,0 +1,14 @@
+package org.la.corejava.student.aameen;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+		// wap to print number as char and its ascii value 
+		
+		char v = '2';
+		System.out.println("char number : "+v);
+		System.out.println("ASCII value: " +(int)v);
+
+	}
+
+}
