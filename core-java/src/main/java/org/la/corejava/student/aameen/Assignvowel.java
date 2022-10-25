d79408161208a77bc6e45df91ed978da51cd8a1f
AAMEEN <AAMEEN@LAPTOP-TPIV9VVE.domain.name>
2022-10-25 20:44:38 +0530

    declare 5 char variable and assign vowel and print it

---
+++ core-java/src/main/java/org/la/corejava/student/aameen/Assignvowel.java
@@ -0,0 +1,13 @@
+package org.la.corejava.student.aameen;
+
+public class Assignvowel {
+
+	public static void main(String[] args) {
+		// declare 5 char and assign vowel to print 
+		char a = 'a',b='e',c='i',d='o',e='u';
+		System.out.println("The assign vowel is : " +a +";" +b+";"+c+";"+d+";"+e);
+		
+
+	}
+
+}
