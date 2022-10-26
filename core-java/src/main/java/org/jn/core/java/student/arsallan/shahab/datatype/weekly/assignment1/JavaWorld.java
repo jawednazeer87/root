86e8c016e8f6498d6fa30002013697f74b45f8d9
Arsallan Shahab <mdarsallan@gmail.com>
2022-10-26 21:28:21 +0530

    pushing weekly assignment

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/JavaWorld.java
@@ -0,0 +1,9 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class JavaWorld {
+
+	public static void main(String[] args) {
+		System.out.println("java world");
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/LongVariable.java
@@ -0,0 +1,8 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class LongVariable {
+	public static void main(String[] args) {
+		long a = 1234567891;
+		System.out.println("value of  long vriable is "+ a);
+	}
+}
