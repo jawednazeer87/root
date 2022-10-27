a11bf04c5d4729577793c3b8b937e1883a718b8b
Arsallan Shahab <mdarsallan@gmail.com>
2022-10-27 23:04:44 +0530

    pushing  weekly assignment

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/DoubleVariables.java
@@ -0,0 +1,8 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class DoubleVariables {
+	public static void main(String[] args) {
+		double a = 21988239.883787328990d, b=8378932.2767287236823829d;
+		System.out.println("value of double variables a ="+a+" & b = "+b);
+	}
+}
