8bc34f7e505dca81c3e7e9c0e46e95a5703dde61
Arsallan Shahab <mdarsallan@gmail.com>
2022-10-27 23:20:50 +0530

    pushing weekly assignmentC

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/CharVariables.java
@@ -0,0 +1,8 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class CharVariables {
+	public static void main(String[] args) {
+		char a = 'H', b='A';
+		System.out.println("value of char variables a = "+a+" & b = "+b);
+	}
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/FloatAndIntVariable.java
@@ -0,0 +1,9 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class FloatAndIntVariable {
+	public static void main(String[] args) {
+		float a = 873.928928f;
+		int b = 92893;
+		System.out.println("vlaue of float varibale is " + a +" and int variable is "+b);
+	}
+}
