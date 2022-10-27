6797765696111520844a6cba95b668b12af90dc7
Sadullah <Sadullah@localhost>
2022-10-27 20:25:44 +0530

    Weekly Assignment Push

---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/ThreeCharVariables.java
@@ -0,0 +1,10 @@
+package org.jn.core.java.student.sadullah.weekly.assignment1;
+
+public class ThreeCharVariables {
+
+	public static void main(String[] args) {
+		char A = 'D', B = 'E', C = 'F';
+		System.out.println("A = " +A +", B = " +B + ", C = " +C);
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/ThreeFloatVariables.java
@@ -0,0 +1,10 @@
+package org.jn.core.java.student.sadullah.weekly.assignment1;
+
+public class ThreeFloatVariables {
+
+	public static void main(String[] args) {
+		float a = 99.256f, b = 435.12f, c = 8734.12f;
+		System.out.println("a = " +a +", b = " +b + ", c = " +c);
+	}
+
+}
