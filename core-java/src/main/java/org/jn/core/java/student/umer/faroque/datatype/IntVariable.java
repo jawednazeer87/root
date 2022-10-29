8f5b3255b71786f9bb866d9f77bd3d357634ff3f
umer <umerfaroque20@gmail.com>
2022-10-30 02:45:54 +0530

    int variable prgrm

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/datatype/IntVariable.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.datatype;
+
+/**
+ * @author umer
+ *
+ */
+public class IntVariable {
+
+	public static void main(String[] args) {
+		//WAP to create and print an int variable
+		int a=10;
+		System.out.println("Int Variable a = "+a);
+
+	}
+
+}
