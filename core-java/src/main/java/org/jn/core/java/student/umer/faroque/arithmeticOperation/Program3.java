224543afb72464340c3efccd22a71eb329aba74b
umer <umerfaroque20@gmail.com>
2022-10-30 03:55:56 +0530

    10 programs in arithmetic operation

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/arithmeticOperation/Program3.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.arithmeticOperation;
+
+/**
+ * @author umerf
+ *
+ */
+public class Program3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to subtract two numbers?
+		int num1=65;
+		int num2=6;
+		System.out.println("Diff = "+(num1-num2));
+
+	}
+
+}
