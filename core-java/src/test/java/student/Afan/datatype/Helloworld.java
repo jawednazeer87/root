bd7519969da501bff6964843f346041de0517a54
Mohammed Afan <Mohammed Afan@LAPTOP-EH76GOUE>
2022-10-30 00:04:17 +0530

    Firstprog

---
+++ core-java/src/test/java/student/Afan/datatype/Helloworld.java
@@ -0,0 +1,10 @@
+package student.Afan.datatype;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This afan");
+	}
+
+}
