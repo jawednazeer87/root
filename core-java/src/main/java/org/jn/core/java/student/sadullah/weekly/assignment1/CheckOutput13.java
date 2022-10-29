02d3c91ba4b7fe697aca7e0b42f80c309b7dc937
Sadullah <Sadullah@localhost>
2022-10-29 19:43:14 +0530

    Weekly assignment push

---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/CheckOutput13.java
@@ -0,0 +1,15 @@
+package org.jn.core.java.student.sadullah.weekly.assignment1;
+
+public class CheckOutput13 {
+
+	public static void main(String[] args) {
+		int a = 34;
+        double d = 20.5;
+        d+=a;
+        a+=d;
+        d-=a;
+        a-=d;
+        System.out.println(" d = " + ++d );
+        System.out.println(" a = " + a-- );
+	}
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/CheckOutput8.java
@@ -0,0 +1,11 @@
+package org.jn.core.java.student.sadullah.weekly.assignment1;
+
+public class CheckOutput8 {
+
+	public static void main(String[] args) {
+		int x = 42;
+        double y = 42.25;
+		System.out.println("x mod 10 = " + x % 10 );
+        System.out.println("y mod 10 = " + y % 10 );
+	}
+}
