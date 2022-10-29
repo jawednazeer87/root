005b4b6723a10dc682b29d8b381a86237aa9d4ff
umer <umerfaroque20@gmail.com>
2022-10-30 03:34:53 +0530

    TypeCasting completed

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/typecasting/IntCharCasting.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.typecasting;
+
+/**
+ * @author umerf
+ *
+ */
+public class IntCharCasting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to create a char variable but assign an int value and print that variable?
+		char a = 69;
+		System.out.println("Char variable is "+a);
+
+	}
+
+}
