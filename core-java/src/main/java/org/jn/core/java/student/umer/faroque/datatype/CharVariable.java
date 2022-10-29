ebe50e63f8b1b254596cf27b6283f331c6421b1a
umer <umerfaroque20@gmail.com>
2022-10-30 03:13:19 +0530

    3 datatypes programs added

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/datatype/CharVariable.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.datatype;
+
+/**
+ * @author umerf
+ *
+ */
+public class CharVariable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to create and print a char variable
+		char a='B';
+		System.out.println("Int Variable a = "+a);
+
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/datatype/ShortVariable.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.datatype;
+
+/**
+ * @author umerf
+ *
+ */
+public class ShortVariable {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s=1050;
+		System.out.println("s = "+s);
+
+	}
+
+}
