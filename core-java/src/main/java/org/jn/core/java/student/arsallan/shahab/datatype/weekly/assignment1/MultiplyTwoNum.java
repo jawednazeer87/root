e92a1df8015f98dd23c9b8d77f75553ca05a6815
Arsallan Shahab <mdarsallan@gmail.com>
2022-10-30 17:24:33 +0530

    pushing weekly assignment

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment1/MultiplyTwoNum.java
@@ -0,0 +1,8 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment1;
+
+public class MultiplyTwoNum {
+	public static void main(String[] args) {
+		int a = 12, b = 3;
+		System.out.println("a*b: "+(a*b));
+	}
+}
