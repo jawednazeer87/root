949010b177ac0d17c3b200b633b6588a206d7ea7
geeksyed <sayedhassain14@gmail.com>
2022-10-30 15:00:27 +0530

    only 5 increment decrement completed

---
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/incrementDecrement/Program5.java
@@ -0,0 +1,15 @@
+package org.jn.core.java.student.sayed.hassain.incrementDecrement;
+
+public class Program5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// wap to pre increment a number n multiply with a number and assign product to a variable, print both variables
+		
+		int a = 8;
+		int b = ++a * 6;
+		System.out.println("a: " + a + " b: " + b); // 9 54
+	}
+
+}
