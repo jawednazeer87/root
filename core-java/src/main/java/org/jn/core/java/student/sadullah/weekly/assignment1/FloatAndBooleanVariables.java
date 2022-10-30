80c566b73462c08ec62539b4ca82b4e2f93aa0d5
Sadullah <Sadullah@localhost>
2022-10-30 18:51:09 +0530

    Weekly asiignment push

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/FloatAndBooleanVariables.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment1/FloatAndBooleanVariables.java
@@ -1,3 +1,4 @@
+package org.jn.core.java.student.sadullah.weekly.assignment1;
 
 public class FloatAndBooleanVariables {
 
@@ -5,7 +6,5 @@ public class FloatAndBooleanVariables {
 		float fl=9986.725f;
 		boolean boo = true;
 		System.out.println("The declared float variable is " + fl +" & the declared boolean variable is " +boo );
-
 	}
-
 }
