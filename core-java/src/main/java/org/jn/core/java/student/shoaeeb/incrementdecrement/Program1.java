55d0230e378552e32a81bfea76b3d6edc88eaf10
shoaeeb <osman.shoaeeb@gmail.com>
2022-10-30 13:37:07 +0530

    Weekly Assignment

---
+++ core-java/src/main/java/org/jn/core/java/student/shoaeeb/incrementdecrement/Program1.java
@@ -0,0 +1,14 @@
+package org.jn.core.java.student.shoaeeb.incrementdecrement;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=1;
+		++num;
+		int number=num;
+		System.out.println(num + ","+number);
+
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/shoaeeb/incrementdecrement/Question14.java
@@ -0,0 +1,16 @@
+package org.jn.core.java.student.shoaeeb.incrementdecrement;
+
+public class Question14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		int y = -2;
+		int z = ++x * y++;
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		
+	}
+
+}
