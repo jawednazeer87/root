0165c6a0b4dc24b8345c43cc16f82b8571e5c1b8
umer <umerfaroque20@gmail.com>
2022-10-30 05:36:11 +0530

    Arithmetic Operations completed

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/arithmeticOperation/Program26.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.arithmeticOperation;
+
+/**
+ * @author umerf
+ *
+ */
+public class Program26 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s = 10;
+        int a = s;		//10
+        s = (short) (s + 10);	//20
+        System.out.println(s + "," + a);	//20,10
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/arithmeticOperation/Program29.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.arithmeticOperation;
+
+/**
+ * @author umerf
+ *
+ */
+public class Program29 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to add two int variable but assign the result to float variable and print the result?
+		int num1=2, num2=7;
+		float d= num1+num2;
+		System.out.println(d);	//9.0
+
+	}
+
+}
