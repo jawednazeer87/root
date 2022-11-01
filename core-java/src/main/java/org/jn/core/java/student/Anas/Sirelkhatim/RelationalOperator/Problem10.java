f91e094a0e2e33eb77a3816af17dec39952e2144
Anas <sirelkhatimanas@gmail.com>
2022-11-01 12:34:53 +0800

    Assignment programs for Relational Operator

---
+++ core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/RelationalOperator/Problem10.java
@@ -0,0 +1,14 @@
+package org.jn.core.java.student.Anas.Sirelkhatim.RelationalOperator;
+
+public class Problem10 {
+
+    public static void main(String[] args) {
+        
+        int a = -1;
+        boolean x = a!=-2 && ++a==0;
+        System.out.println(a);
+        System.out.println(x);
+
+    }
+
+}
