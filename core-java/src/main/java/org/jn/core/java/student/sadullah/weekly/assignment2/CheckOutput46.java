dc9e429d48d4c677a05e5fd79c93959ea5d185bf
Sadullah <Sadullah@localhost>
2022-11-04 22:57:32 +0530

    Weekly assignment push

---
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment2/CheckOutput46.java
@@ -0,0 +1,17 @@
+package org.jn.core.java.student.sadullah.weekly.assignment2;
+
+public class CheckOutput46 {
+
+	public static void main(String[] args) {
+		int a = 12 + 21 * 3 - 9 / 2;
+        int b = 14 - 32 * 4 + 175 / 8 - 3;
+        if(++a > 71 && --b < 20)
+        {
+            System.out.println("a = " + a + " b = " + b);
+        }
+        if(b-- == -97 || a-- < 100)
+        {
+            System.out.println("a = " + a + " b = " + b);
+        }
+	}
+}
