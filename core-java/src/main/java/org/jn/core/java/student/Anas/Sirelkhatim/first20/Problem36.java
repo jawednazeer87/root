fe46369a0a592b421c310c018223e5873b050622
Anas <sirelkhatimanas@gmail.com>
2022-11-13 20:02:18 +0800

    codes for assingments

--- core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/first20/Problem36.java
+++ core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/first20/Problem36.java
@@ -3,7 +3,8 @@ package org.jn.core.java.student.Anas.Sirelkhatim.first20;
 public class Problem36 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+       
+        
 
     }
 
--- core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/first20/Problem37.java
+++ core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/first20/Problem37.java
@@ -3,7 +3,17 @@ package org.jn.core.java.student.Anas.Sirelkhatim.first20;
 public class Problem37 {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        
+        int i = 1;
+        
+        while(i <= 10)
+        {
+            if(i == 3 || i == 7)
+                i++;
+            System.out.println(i);
+            i++;
+                
+        }
 
     }
 
