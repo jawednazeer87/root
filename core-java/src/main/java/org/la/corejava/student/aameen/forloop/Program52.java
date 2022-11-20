02a0568f35a3d9135e2c4b0e636e8ca7667fd45a
AAMEEN <AAMEEN@LAPTOP-TPIV9VVE.domain.name>
2022-11-20 18:12:51 +0530

    Week 4

---
+++ core-java/src/main/java/org/la/corejava/student/aameen/forloop/Program52.java
@@ -0,0 +1,22 @@
+package org.la.corejava.student.aameen.forloop;
+import java.util.Scanner;
+public class Program52 {
+
+	public static void main(String[] args) {
+		// Wap to find sum of all even numbers between 1 to n.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the amount of natural number : ");
+		int n = sc.nextInt();
+		int sum=0;
+		for(int i = 1; i<=n;i++)
+		{
+			if(i%2==0)
+			{
+				sum=sum+i;
+			}
+		}
+		System.out.println("The sum of all even natural number between 1 to "+n+ " : " +sum);
+		
+	}
+
+}
