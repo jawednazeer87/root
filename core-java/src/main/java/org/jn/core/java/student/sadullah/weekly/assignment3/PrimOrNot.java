3e6042566dcb928d2927a132047cf928bc7de65c
Sadullah <Sadullah@192.168.1.6>
2022-12-02 09:59:04 +0530

    Last assignment push.

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment3/PrimOrNot.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment3/PrimOrNot.java
@@ -3,7 +3,7 @@ package org.jn.core.java.student.sadullah.weekly.assignment3;
 public class PrimOrNot {
 
 	public static void main(String[] args) {
-		int n=11,c=0;
+		int n=3,c=0;
 		for(int i=1;i<n;i++)
 		{
 		if(n%i==0)
