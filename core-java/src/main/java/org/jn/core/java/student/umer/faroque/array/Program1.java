4e73f4dc391903f58dc7ec21681c2362f3450322
umer <umerfaroque20@gmail.com>
2022-12-04 17:40:56 +0530

    Arrays

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/array/Program1.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.array;
+
+/**
+ * @author umerf
+ *
+ */
+public class Program1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to create and print array using without loop
+int [] a = {12,14,15,18,30};
+		
+		System.out.println("a[0] = " + a[0]);
+		System.out.println("a[1] = " + a[1]);
+		System.out.println("a[2] = " + a[2]);
+		System.out.println("a[3] = " + a[3]);
+		System.out.println("a[4] = " + a[4]);
+
+	}
+
+}
