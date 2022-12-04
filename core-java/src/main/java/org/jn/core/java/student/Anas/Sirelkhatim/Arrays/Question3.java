d37b45c799ee8a7cb03d4b27e43c798dcf115f43
Anas <sirelkhatimanas@gmail.com>
2022-12-04 19:20:17 +0800

    Programs for arrays probelm questions

---
+++ core-java/src/main/java/org/jn/core/java/student/Anas/Sirelkhatim/Arrays/Question3.java
@@ -0,0 +1,12 @@
+package org.jn.core.java.student.Anas.Sirelkhatim.Arrays;
+
+public class Question3 {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[20];
+        System.out.println(arr.length);
+
+    }
+
+}
