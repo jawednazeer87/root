848981f71f3b83e963e9f3630101163e7fd898e9
umer <umerfaroque20@gmail.com>
2022-12-04 17:46:55 +0530

    arrays

---
+++ core-java/src/main/java/org/jn/core/java/student/umer/faroque/array/Program2.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.jn.core.java.student.umer.faroque.array;
+
+/**
+ * @author umerf
+ *
+ */
+public class Program2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// wap to create and print array using loop
+		int [] a = {1,4,5,8,3};
+		for(int i = 0; i < a.length; i++)
+        {
+           System.out.println(a[i]);
+        }
+
+	}
+
+}
