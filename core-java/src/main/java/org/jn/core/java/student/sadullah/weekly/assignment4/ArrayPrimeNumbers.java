50bf17fa0ecedd30a73a8eff710788ca016728dc
Sadullah <Sadullah@192.168.1.8>
2022-12-07 18:40:55 +0530

    Weekly

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
@@ -5,8 +5,8 @@ public class ArrayPrimeNumbers {
 		int a [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] ) {
-				
+			if (a[i] == 1 ) {
+				System.out.println("Prime");
 			}
 		}
 	}
