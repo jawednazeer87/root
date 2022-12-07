d24a32d090e1341489e314fa39cd244f61d49282
Sadullah <Sadullah@192.168.1.8>
2022-12-07 17:59:13 +0530

    weekly

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
@@ -3,5 +3,6 @@ package org.jn.core.java.student.sadullah.weekly.assignment4;
 public class ArrayPrimeNumbers {
 	public static void main(String[] argrs) {
 		int a [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int b [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	}
 }
