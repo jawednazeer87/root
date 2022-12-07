b12f835934482ecfd4a682176df74d527257ea3a
Sadullah <Sadullah@192.168.1.8>
2022-12-07 20:40:01 +0530

    Weekly assignment pushes

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayPrimeNumbers.java
@@ -2,12 +2,28 @@ package org.jn.core.java.student.sadullah.weekly.assignment4;
 
 public class ArrayPrimeNumbers {
 	public static void main(String[] argrs) {
-		int a [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		
+		int a [] = {1, 2, 3, 4, 5};
+
 		for (int i = 0; i < a.length; i++) {
-			if (a[i] == 1 ) {
-				System.out.println("Prime");
-			}
-		}
+	        boolean isPrime = true;
+	        if (a[i] == 1)
+	            isPrime = false;
+	        else {
+	            // check to see if the numbers are prime
+	            for (int j = 2; j <= a[i] / 2; j++) {
+	                if (a[i] % j == 0) {
+	                    isPrime = false;
+	                    break;
+	                }
+	            }
+	        }
+	        // print the number
+	        if (isPrime){
+	            if (a[i] == 0){}
+	            else {
+	                System.out.print(a[i] + " , ");
+	            }
+	    }}
+	    System.out.println(" are the prime numbers in the array ");
 	}
 }
--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArraySame.java
+++
@@ -1,5 +0,0 @@
-package org.jn.core.java.student.sadullah.weekly.assignment4;
-
-public class ArraySame {
-
-}
