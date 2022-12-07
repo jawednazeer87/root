d4028ba8f3e4ed2d993cd4f29fab1cc351fa8e25
Sadullah <Sadullah@192.168.1.8>
2022-12-07 14:52:38 +0530

    Weekly assignment push

--- core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayCheckIfSameOrNot.java
+++ core-java/src/main/java/org/jn/core/java/student/sadullah/weekly/assignment4/ArrayCheckIfSameOrNot.java
@@ -4,19 +4,7 @@ public class ArrayCheckIfSameOrNot {
 	public static void main(String[] argrs) {
 		int a [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int b [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		
-		if (a.length == b.length) {
-			for (int i = 0; i < b.length; i++) {
-				if (a[i] == b [i]) {
-					System.out.println("Both arrays are equal.");
-				}
-				else {
-					System.out.println("Arrays are not equal");
-				}
-			}
-		}
-		else {
-			System.out.println("Arrays are not equal");
-		}
+		boolean same;
+
 	}
 }
