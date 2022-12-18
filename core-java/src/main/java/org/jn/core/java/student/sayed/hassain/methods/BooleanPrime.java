cd03a70f37bce91a98810eaa1cca1f212c40d9b3
geeksyed <sayedhassain14@gmail.com>
2022-12-18 14:53:09 +0530

    methods

---
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/methods/BooleanPrime.java
@@ -0,0 +1,26 @@
+package org.jn.core.java.student.sayed.hassain.methods;
+
+import java.util.Scanner;
+
+public class BooleanPrime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter nummber: ");
+		int a = sc.nextInt();
+		boolean res = prime(a);
+		System.out.println(a + " is Prime Nummber: " + res);
+	}
+
+	static boolean prime(int x) {
+		if (x == 1)
+			return false;
+		for (int i = 2; i <= x / 2; i++)
+			if (x % i == 0)
+				return false;
+		return true;
+	}
+
+}
---
+++ core-java/src/main/java/org/jn/core/java/student/sayed/hassain/methods/CheckChar.java
@@ -0,0 +1,25 @@
+package org.jn.core.java.student.sayed.hassain.methods;
+
+import java.util.Scanner;
+
+public class CheckChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Character: ");
+		char ch = sc.next().charAt(0);
+		System.out.println(checkChar(ch));
+	}
+
+	static int checkChar(char c) {
+		if (Character.isUpperCase(c))
+			return 1;
+		else if (Character.isLowerCase(c))
+			return -1;
+		else
+			return 0;
+	}
+
+}
