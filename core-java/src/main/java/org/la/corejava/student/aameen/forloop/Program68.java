c51f4936c199ad4c8550147a870d412cb1c0e03e
AAMEEN <AAMEEN@LAPTOP-TPIV9VVE>
2022-12-31 12:31:17 +0530

    Week 4

---
+++ core-java/src/main/java/org/la/corejava/student/aameen/forloop/Program68.java
@@ -0,0 +1,31 @@
+package org.la.corejava.student.aameen.forloop;
+import java.util.Scanner;
+public class Program68 {
+
+	public static void main(String[] args) {
+		// Wap to print all Prime numbers between 1 to n
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number : ");
+		int num=sc.nextInt();
+		
+		System.out.println("Prime number from 1 to "+num);
+		for(int i = 2;i<=num;i++)
+		{
+			boolean isPrime = true;
+			for(int n = 2;n<i;n++)
+			{
+				if(i%n==0)
+				{
+					isPrime=false;
+					break ;
+				}
+			}
+			if(isPrime==true)
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
