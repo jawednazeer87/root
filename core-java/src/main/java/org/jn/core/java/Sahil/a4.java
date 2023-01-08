6fe6de1c92c6cc9da9511d0f9073c6a227515467
sahilhaider962002 <sahilhaider932@gmail.com>
2023-01-08 17:58:06 +0530

    a

---
+++ core-java/src/main/java/org/jn/core/java/Sahil/a4.java
@@ -0,0 +1,22 @@
+package org.jn.core.java.Sahil;
+
+public class a4 {
+static void printNegative  (int[] arr) {
+	
+	for(int x:arr) {
+		if(x<0) {
+		System.out.println("  "+x);
+		}
+	
+	
+}
+}
+
+
+
+public static void main(String[] args) {
+	int arr[]= {-23,-98,77};
+	printNegative(arr);
+}
+
+}
