b0528168bf77b4ed42b2127742b1aa376285af88
arsallanShahab <mdarsallan@gmail.com>
2023-01-14 01:33:44 +0530

    checking push

--- core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment4/first.java
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment4/first.java
@@ -2,6 +2,6 @@ package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment4;
 
 public class first {
 	public static void main(String[] args) {
-		System.out.println("ARSALLAN");
+		System.out.println("ARSALLAN SHAHAB");
 	}
 }
