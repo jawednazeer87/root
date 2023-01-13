f42bab3bb794824125a96ee28956133112935ae1
BEHIDE <BEHIDE@DESKTOP-GMV9LF1>
2023-01-14 01:16:25 +0530

    checking github push

---
+++ core-java/src/main/java/org/jn/core/java/student/arsallan/shahab/datatype/weekly/assignment4/first.java
@@ -0,0 +1,7 @@
+package org.jn.core.java.student.arsallan.shahab.datatype.weekly.assignment4;
+
+public class first {
+	public static void main(String[] args) {
+		System.out.println("ARSALLAN");
+	}
+}
