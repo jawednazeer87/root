d6596ff4dfc2bc3a52ff2df9a5856a4ee12a93cd
admin <admin@DESKTOP-K2163UI-AFIRA>
2023-07-16 07:27:21 +0530

    Consume REST API

--- student-3/src/main/java/org/student/three/mirza/rest/controller/ConsumesRestController.java
+++ student-3/src/main/java/org/student/three/mirza/rest/controller/ConsumesRestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.student.three.dto.ConsumeDTO;
+
 import org.student.three.dto.ConsumerListDTO;
 import org.student.three.mirza.modal.ConsumeModel;
 
